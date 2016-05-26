89da132450677ae69aef156ade4c3a11e3885a33
Jonas Helfer <jonas@helfer.email>
2016-05-25 17:51:21 -0700

    merge in tracer changes from graphql-tools manually

--- src/apolloServer.js
+++ src/apolloServer.js
@@ -3,9 +3,11 @@ import {
   buildSchemaFromTypeDefinitions,
   addErrorLoggingToSchema,
   addCatchUndefinedToSchema,
+  addResolveFunctionsToSchema,
+  addTracingToResolvers,
 } from 'graphql-tools';
 import { addMockFunctionsToSchema } from 'graphql-tools';
-import graphqlHTTP from 'express-graphql';
+import graphqlHTTP from 'express-widgetizer';
 import { GraphQLSchema, formatError } from 'graphql';
 
 // TODO this implementation could use a bit of refactoring.
@@ -20,8 +22,14 @@ export default function apolloServer(options, ...rest) {
     throw new Error(`apolloServer expects exactly one argument, got ${rest.length + 1}`);
   }
   // Resolve the Options to get OptionsData.
+
   return (req, res) => {
-    new Promise(resolve => {
+    let tracerLogger;
+
+    // TODO instrument ApolloServer's schema creation as well, so you know how long
+    // it takes. May be a big waste of time to recreate the schema for every request.
+
+    return new Promise(resolve => {
       resolve(typeof options === 'function' ? options(req) : options);
     }).then(optionsData => {
       // Assert that optionsData is in fact an Object.
@@ -42,18 +50,32 @@ export default function apolloServer(options, ...rest) {
         resolvers, // required if mocks is not false and schema is not GraphQLSchema
         connectors, // required if mocks is not false and schema is not GraphQLSchema
         logger,
+        tracer,
         printErrors,
         mocks = false,
         allowUndefinedInResolve = true,
         pretty, // pass through
         graphiql = false, // pass through
         validationRules, // pass through
-        context = {}, // pass through
+        context = {}, // pass through, but add tracer if applicable
         rootValue, // pass through
       } = optionsData;
 
       // would collide with formatError from graphql otherwise
       const formatErrorFn = optionsData.formatError;
+
+      // TODO: currently relies on the fact that start and end both exist
+      // and appear in the correct order and exactly once.
+      function processInterval(supertype, subtype, tstamp, intervalMap) {
+        if (subtype === 'start') {
+          // eslint-disable-next-line no-param-reassign
+          intervalMap[supertype] = tstamp;
+        }
+        if (subtype === 'end') {
+          // eslint-disable-next-line no-param-reassign
+          intervalMap[supertype] = tstamp - intervalMap[supertype];
+        }
+      }
 
       let executableSchema;
       if (mocks) {
@@ -61,6 +83,7 @@ export default function apolloServer(options, ...rest) {
         // have to rewrite these functions
         const myMocks = mocks || {};
         executableSchema = buildSchemaFromTypeDefinitions(schema);
+        addResolveFunctionsToSchema(executableSchema, resolvers || {});
         addMockFunctionsToSchema({
           schema: executableSchema,
           mocks: myMocks,
@@ -97,6 +120,98 @@ export default function apolloServer(options, ...rest) {
           }
         }
       }
+
+      // Tracer-related stuff ------------------------------------------------
+
+      tracerLogger = { log: undefined, report: undefined };
+      if (tracer) {
+        tracerLogger = tracer.newLoggerInstance();
+        tracerLogger.log('request.info', {
+          headers: req.headers,
+          baseUrl: req.baseUrl,
+          originalUrl: req.originalUrl,
+          method: req.method,
+          httpVersion: req.httpVersion,
+          remoteAddr: req.connection.remoteAddress,
+        });
+        if (context.tracer) {
+          throw new Error('Property tracer on context already defined, cannot attach Tracer');
+        } else {
+          context.tracer = tracerLogger;
+        }
+        if (!executableSchema._apolloTracerApplied) {
+          addTracingToResolvers(executableSchema);
+        }
+      }
+
+      // TODO: move to proper place, make less fragile ...
+      // calculate timing information from events
+      function timings(events) {
+        const resolverDurations = [];
+        const intervalMap = {};
+
+        // split by event.type = [ , ]
+        events.forEach(e => {
+          const [supertype, subtype] = e.type.split('.');
+          switch (supertype) {
+            case 'request':
+            case 'parse':
+            case 'validation':
+            case 'execution':
+            case 'parseBody':
+            case 'parseParams':
+              processInterval(supertype, subtype, e.timestamp, intervalMap);
+              break;
+            case 'resolver':
+              if (subtype === 'end') {
+                resolverDurations.push({
+                  type: 'resolve',
+                  functionName: e.data.functionName,
+                  duration: e.timestamp - events[e.data.startEventId].timestamp,
+                });
+              }
+              break;
+            default:
+              console.error(`Unknown event type ${supertype}`);
+          }
+        });
+
+        const durations = [];
+        Object.keys(intervalMap).forEach((key) => {
+          durations.push({
+            type: key,
+            functionName: null,
+            duration: intervalMap[key],
+          });
+        });
+        return durations.concat(resolverDurations);
+      }
+
+      let extensionsFn = function extensionsFn() {
+        try {
+          return {
+            timings: timings(tracerLogger.report().events),
+            tracer: tracerLogger.report().events.map(e => ({
+              id: e.id,
+              type: e.type,
+              ts: e.timestamp,
+              data: e.data,
+            })).filter(x => x.type !== 'initialization'),
+          };
+        } catch (e) {
+          console.error(e);
+          console.error(e.stack);
+        }
+        return {};
+      };
+
+      // XXX ugly way of only passing extensionsFn when tracer is defined.
+      if (!tracer || req.headers['x-apollo-tracer-extension'] !== 'on') {
+        extensionsFn = undefined;
+      }
+
+      // end of Tracer related stuff -------------------------------------------
+
       // graphQLHTTPOptions
       return {
         schema: executableSchema,
@@ -106,6 +221,8 @@ export default function apolloServer(options, ...rest) {
         context,
         rootValue,
         graphiql,
+        logFn: tracerLogger.log,
+        extensionsFn,
       };
     }).then((graphqlHTTPOptions) => {
       return graphqlHTTP(graphqlHTTPOptions)(req, res);
@@ -119,6 +236,10 @@ export default function apolloServer(options, ...rest) {
       res
         .set('Content-Type', 'application/json')
         .send(JSON.stringify(result));
+      return result;
+    }).then(() => {
+      // send traces to Apollo Tracer
+      tracerLogger.submit();
     });
   };
 }
