76379b2dda8d96f6510ac3c570586972b0a05f61
nnance <nance.nick@gmail.com>
2016-07-18 21:13:57 -0700

    fix bad merge

--- src/integrations/integrations.test.ts
+++ src/integrations/integrations.test.ts
@@ -68,191 +68,194 @@ export interface CreateAppFunc {
 
 export default (createApp: CreateAppFunc) => {
   describe('apolloServer', () => {
-    it('can be called with an options function', () => {
-        const app = createApp({apolloOptions: (): ApolloOptions => ({schema: Schema})});
-        const expected = {
-            testString: 'it works',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test{ testString }',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+    describe('graphqlHTTP', () => {
+      it('can be called with an options function', () => {
+          const app = createApp({apolloOptions: (): ApolloOptions => ({schema: Schema})});
+          const expected = {
+              testString: 'it works',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testString }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-    it('can be called with an options function that returns a promise', () => {
-        const app = createApp({ apolloOptions: () => {
-            return new Promise(resolve => {
-                resolve({schema: Schema});
-            });
-        }});
-        const expected = {
-            testString: 'it works',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test{ testString }',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can be called with an options function that returns a promise', () => {
+          const app = createApp({ apolloOptions: () => {
+              return new Promise(resolve => {
+                  resolve({schema: Schema});
+              });
+          }});
+          const expected = {
+              testString: 'it works',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testString }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-    it('throws an error if options promise is rejected', () => {
-        const app = createApp({ apolloOptions: () => {
-          return Promise.reject({}) as any as ApolloOptions;
-        }});
-        const expected = 'Invalid options';
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test{ testString }',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(500);
-            return expect(res.error.text).to.contain(expected);
-        });
-    });
+      it('throws an error if options promise is rejected', () => {
+          const app = createApp({ apolloOptions: () => {
+            return Promise.reject({}) as any as ApolloOptions;
+          }});
+          const expected = 'Invalid options';
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testString }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(500);
+              return expect(res.error.text).to.contain(expected);
+          });
+      });
 
-    it('throws an error if POST body is missing', () => {
-        const app = createApp({excludeParser: true});
-        const req = request(app)
-            .post('/graphql')
-            .send();
-        return req.then((res) => {
-            expect(res.status).to.equal(500);
-            return expect(res.error.text).to.contain('POST body missing.');
-        });
-    });
+      it('throws an error if POST body is missing', () => {
+          const app = createApp({excludeParser: true});
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testString }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(500);
+              return expect(res.error.text).to.contain('POST body missing.');
+          });
+      });
 
 
-    it('can handle a basic request', () => {
-        const app = createApp();
-        const expected = {
-            testString: 'it works',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test{ testString }',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can handle a basic request', () => {
+          const app = createApp();
+          const expected = {
+              testString: 'it works',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testString }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-    it('can handle a request with variables', () => {
-        const app = createApp();
-        const expected = {
-            testArgument: 'hello world',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test($echo: String){ testArgument(echo: $echo) }',
-                variables: { echo: 'world' },
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can handle a request with variables', () => {
+          const app = createApp();
+          const expected = {
+              testArgument: 'hello world',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test($echo: String){ testArgument(echo: $echo) }',
+                  variables: { echo: 'world' },
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-    it('can handle a request with variables as string', () => {
-        const app = createApp();
-        const expected = {
-            testArgument: 'hello world',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'query test($echo: String!){ testArgument(echo: $echo) }',
-                variables: '{ "echo": "world" }',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can handle a request with variables as string', () => {
+          const app = createApp();
+          const expected = {
+              testArgument: 'hello world',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test($echo: String!){ testArgument(echo: $echo) }',
+                  variables: '{ "echo": "world" }',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-    it('can handle a request with operationName', () => {
-        const app = createApp();
-        const expected = {
-            testString: 'it works',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: `
-                    query test($echo: String){ testArgument(echo: $echo) }
-                    query test2{ testString }`,
-                variables: { echo: 'world' },
-                operationName: 'test2',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can handle a request with operationName', () => {
+          const app = createApp();
+          const expected = {
+              testString: 'it works',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: `
+                      query test($echo: String){ testArgument(echo: $echo) }
+                      query test2{ testString }`,
+                  variables: { echo: 'world' },
+                  operationName: 'test2',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-     it('can handle batch requests', () => {
-        const app = createApp();
-        const expected = [
-            {
-                data: {
-                    testString: 'it works',
-                },
-            },
-            {
-                data: {
-                    testArgument: 'hello yellow',
-                },
-            },
-        ];
-        const req = request(app)
-            .post('/graphql')
-            .send([{
-                query: `
-                    query test($echo: String){ testArgument(echo: $echo) }
-                    query test2{ testString }`,
-                variables: { echo: 'world' },
-                operationName: 'test2',
-            },
-            {
-                query: `
-                    query testX($echo: String){ testArgument(echo: $echo) }`,
-                variables: { echo: 'yellow' },
-                operationName: 'testX',
-            }]);
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body).to.deep.equal(expected);
-        });
-    });
+       it('can handle batch requests', () => {
+          const app = createApp();
+          const expected = [
+              {
+                  data: {
+                      testString: 'it works',
+                  },
+              },
+              {
+                  data: {
+                      testArgument: 'hello yellow',
+                  },
+              },
+          ];
+          const req = request(app)
+              .post('/graphql')
+              .send([{
+                  query: `
+                      query test($echo: String){ testArgument(echo: $echo) }
+                      query test2{ testString }`,
+                  variables: { echo: 'world' },
+                  operationName: 'test2',
+              },
+              {
+                  query: `
+                      query testX($echo: String){ testArgument(echo: $echo) }`,
+                  variables: { echo: 'yellow' },
+                  operationName: 'testX',
+              }]);
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body).to.deep.equal(expected);
+          });
+      });
 
-    it('can handle a request with a mutation', () => {
-        const app = createApp();
-        const expected = {
-            testMutation: 'not really a mutation, but who cares: world',
-        };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                query: 'mutation test($echo: String){ testMutation(echo: $echo) }',
-                variables: { echo: 'world' },
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
-    });
+      it('can handle a request with a mutation', () => {
+          const app = createApp();
+          const expected = {
+              testMutation: 'not really a mutation, but who cares: world',
+          };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'mutation test($echo: String){ testMutation(echo: $echo) }',
+                  variables: { echo: 'world' },
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
       it('applies the formatResponse function', () => {
           const app = createApp({apolloOptions: {
@@ -294,85 +297,84 @@ export default (createApp: CreateAppFunc) => {
 
     });
 
-  });
 
+    describe('renderGraphiQL', () => {
+      it('presents GraphiQL when accepting HTML', () => {
+          const app = createApp({graphiqlOptions: {
+              endpointURL: '/graphql',
+          }});
 
-  describe('renderGraphiQL', () => {
-    it('presents GraphiQL when accepting HTML', () => {
-        const app = createApp({graphiqlOptions: {
-            endpointURL: '/graphql',
-        }});
-
-        const req = request(app)
-            .get('/graphiql?query={test}')
-            .set('Accept', 'text/html');
-        return req.then((response) => {
-            expect(response.status).to.equal(200);
-            expect(response.type).to.equal('text/html');
-            expect(response.text).to.include('{test}');
-            expect(response.text).to.include('/graphql');
-            expect(response.text).to.include('graphiql.min.js');
-        });
-    });
-  });
-
-  describe('stored queries', () => {
-    it('works with formatParams', () => {
-        const store = new OperationStore(Schema);
-        store.put('query testquery{ testString }');
-        const app = createApp({ apolloOptions: {
-            schema: Schema,
-            formatParams(params) {
-                params['query'] = store.get(params.operationName);
-                return params;
-            },
-        }});
-        const expected = { testString: 'it works' };
-        const req = request(app)
-            .post('/graphql')
-            .send({
-                operationName: 'testquery',
-            });
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body.data).to.deep.equal(expected);
-        });
+          const req = request(app)
+              .get('/graphiql?query={test}')
+              .set('Accept', 'text/html');
+          return req.then((response) => {
+              expect(response.status).to.equal(200);
+              expect(response.type).to.equal('text/html');
+              expect(response.text).to.include('{test}');
+              expect(response.text).to.include('/graphql');
+              expect(response.text).to.include('graphiql.min.js');
+          });
+      });
     });
 
-    it('can reject non-whitelisted queries', () => {
-        const store = new OperationStore(Schema);
-        store.put('query testquery{ testString }');
-        const app = createApp({ apolloOptions: {
-            schema: Schema,
-            formatParams(params) {
-                if (params.query) {
-                    throw new Error('Must not provide query, only operationName');
-                }
-                params['query'] = store.get(params.operationName);
-                return params;
-            },
-        }});
-        const expected = [{
-            data: {
-                testString: 'it works',
-            },
-        }, {
-            errors: [{
-                message: 'Must not provide query, only operationName',
-            }],
-        }];
+    describe('stored queries', () => {
+      it('works with formatParams', () => {
+          const store = new OperationStore(Schema);
+          store.put('query testquery{ testString }');
+          const app = createApp({ apolloOptions: {
+              schema: Schema,
+              formatParams(params) {
+                  params['query'] = store.get(params.operationName);
+                  return params;
+              },
+          }});
+          const expected = { testString: 'it works' };
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  operationName: 'testquery',
+              });
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body.data).to.deep.equal(expected);
+          });
+      });
 
-        const req = request(app)
-            .post('/graphql')
-            .send([{
-                operationName: 'testquery',
-            }, {
-                query: '{ testString }',
-            }]);
-        return req.then((res) => {
-            expect(res.status).to.equal(200);
-            return expect(res.body).to.deep.equal(expected);
-        });
+      it('can reject non-whitelisted queries', () => {
+          const store = new OperationStore(Schema);
+          store.put('query testquery{ testString }');
+          const app = createApp({ apolloOptions: {
+              schema: Schema,
+              formatParams(params) {
+                  if (params.query) {
+                      throw new Error('Must not provide query, only operationName');
+                  }
+                  params['query'] = store.get(params.operationName);
+                  return params;
+              },
+          }});
+          const expected = [{
+              data: {
+                  testString: 'it works',
+              },
+          }, {
+              errors: [{
+                  message: 'Must not provide query, only operationName',
+              }],
+          }];
+
+          const req = request(app)
+              .post('/graphql')
+              .send([{
+                  operationName: 'testquery',
+              }, {
+                  query: '{ testString }',
+              }]);
+          return req.then((res) => {
+              expect(res.status).to.equal(200);
+              return expect(res.body).to.deep.equal(expected);
+          });
+      });
     });
   });
 };
