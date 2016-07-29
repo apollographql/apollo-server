790fd30b5db3317a3212399ee30d1af34a459d34
Ian MacLeod <ian@nevir.net>
2016-07-29 10:20:29 -0700

    Expose the Express response object to the options callback (#49)
    For example, it's necessary to have the response object for mutations that set/clear cookies

--- src/integrations/expressApollo.ts
+++ src/integrations/expressApollo.ts
@@ -7,7 +7,7 @@ import ApolloOptions from './apolloOptions';
 import * as GraphiQL from '../modules/renderGraphiQL';
 
 export interface ExpressApolloOptionsFunction {
-  (req?: express.Request): ApolloOptions | Promise<ApolloOptions>;
+  (req?: express.Request, res?: express.Response): ApolloOptions | Promise<ApolloOptions>;
 }
 
 // Design principles:
@@ -33,7 +33,7 @@ export function apolloExpress(options: ApolloOptions | ExpressApolloOptionsFunct
     let optionsObject: ApolloOptions;
     if (isOptionsFunction(options)) {
       try {
-        optionsObject = await options(req);
+        optionsObject = await options(req, res);
       } catch (e) {
         res.statusCode = 500;
         res.write(`Invalid options provided to ApolloServer: ${e.message}`);
