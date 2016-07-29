fe8be5d5ee4690654fe894e586b3893feb750368
Jonas Helfer <helfer@users.noreply.github.com>
2016-07-29 10:18:30 -0700

    add missing graphiql export for Koa (#64)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { apolloExpress, graphiqlExpress } from './integrations/expressApollo';
 export { ApolloHAPI, GraphiQLHAPI } from './integrations/hapiApollo';
-export { apolloKoa } from './integrations/koaApollo';
+export { apolloKoa, graphiqlKoa } from './integrations/koaApollo';
 export { apolloConnect, graphiqlConnect } from './integrations/connectApollo';
