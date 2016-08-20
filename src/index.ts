9cd546060279232e64db4c3817d239324ce71f6b
Nick Nance <nance.nick@gmail.com>
2016-08-20 09:54:47 -0700

    expose interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { runQuery } from './core/runQuery'
 export { renderGraphiQL} from './modules/renderGraphiQL'
-export { apolloExpress, graphiqlExpress } from './integrations/expressApollo';
-export { ApolloHAPI, GraphiQLHAPI } from './integrations/hapiApollo';
-export { apolloKoa, graphiqlKoa } from './integrations/koaApollo';
-export { apolloConnect, graphiqlConnect } from './integrations/connectApollo';
+export { OperationStore } from './modules/operationStore'
+export { apolloExpress, graphiqlExpress } from './integrations/expressApollo'
+export { ApolloHAPI, GraphiQLHAPI } from './integrations/hapiApollo'
+export { apolloKoa, graphiqlKoa } from './integrations/koaApollo'
+export { apolloConnect, graphiqlConnect } from './integrations/connectApollo'
