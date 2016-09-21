b4b39534d83f35f291c7be870477ac66a3b96079
Bojan Hribernik <bojan.hribernik@gmail.com>
2016-09-21 21:21:09 +0200

    Pass ctx instead of ctx.request to koa options function

--- src/integrations/koaApollo.ts
+++ src/integrations/koaApollo.ts
@@ -25,7 +25,7 @@ export function apolloKoa(options: ApolloOptions | KoaApolloOptionsFunction): Ko
     let optionsObject: ApolloOptions;
     if (isOptionsFunction(options)) {
       try {
-        optionsObject = await options(ctx.request);
+        optionsObject = await options(ctx);
       } catch (e) {
         ctx.status = 500;
         return ctx.body = `Invalid options provided to ApolloServer: ${e.message}`;
