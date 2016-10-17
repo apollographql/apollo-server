e2403ed06b71396973e02a50ad00849e8da4f24e
Nicolás López <nicolaslopezj@me.com>
2016-10-17 13:55:15 -0300

    add tests

--- src/integrations/integrations.test.ts
+++ src/integrations/integrations.test.ts
@@ -424,6 +424,31 @@ export default (createApp: CreateAppFunc, destroyApp?: DestroyAppFunc) => {
           });
       });
 
+      it('should use default formatter if provided formatError fails', () => {
+          app = createApp({apolloOptions: {
+              schema: Schema,
+              formatError: (err) => {
+                throw new Error('I should be catched');
+              },
+          }});
+          const logStub = stub(console, 'error');
+          const expected = /at resolveOrError/;
+          app = createApp({apolloOptions: {
+              schema: Schema,
+              debug: true,
+          }});
+          const req = request(app)
+              .post('/graphql')
+              .send({
+                  query: 'query test{ testError }',
+              });
+          return req.then((res) => {
+              logStub.restore();
+              expect(logStub.callCount).to.equal(1);
+              return expect(logStub.getCall(0).args[0]).to.match(expected);
+          });
+      });
+
       it('sends stack trace to error if debug mode is set', () => {
           const expected = /at resolveOrError/;
           const stackTrace = [];
