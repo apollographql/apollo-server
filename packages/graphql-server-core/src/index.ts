6eebf670285c2c0ac8e53e052736d0b2b83675db
Hagai Cohen <hagai.co@gmail.com>
2017-02-08 18:29:22 +0200

    chore(ws-core): created generic options resolve function

--- packages/graphql-server-core/src/index.ts
+++ packages/graphql-server-core/src/index.ts
@@ -1,4 +1,3 @@
 export { runQuery, LogFunction, LogMessage, LogStep, LogAction } from './runQuery';
 export { runHttpQuery, HttpQueryRequest, HttpQueryError } from './runHttpQuery';
 export { default as GraphQLOptions, resolveGraphqlOptions } from './graphqlOptions';
-
