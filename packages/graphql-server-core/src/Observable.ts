728fe7fa6840b551f7c3d36bed7d430bdd105c81
Hagai Cohen <hagai.co@gmail.com>
2017-02-08 21:38:11 +0200

    chore(ws-core): add Observable to graphql-server-core

---
+++ packages/graphql-server-core/src/Observable.ts
@@ -0,0 +1,49 @@
+// This simplified polyfill attempts to follow the ECMAScript Observable proposal.
+// See https://github.com/zenparsing/es-observable
+
+import $$observable from 'symbol-observable';
+
+export type CleanupFunction = () => void;
+export type SubscriberFunction<T> = (observer: Observer<T>) => (Subscription | CleanupFunction);
+
+function isSubscription(subscription: Function | Subscription): subscription is Subscription {
+  return (<Subscription>subscription).unsubscribe !== undefined;
+}
+
+export interface IObservable<T> {
+  subscribe(observer: Observer<T>): Subscription;
+}
+
+export interface Observer<T> {
+  next?: (value: T) => void;
+  error?: (error: Error) => void;
+  complete?: () => void;
+}
+
+export interface Subscription {
+  unsubscribe: CleanupFunction;
+}
+
+export class Observable<T> implements IObservable<T> {
+  private subscriberFunction: SubscriberFunction<T>;
+
+  constructor(subscriberFunction: SubscriberFunction<T>) {
+    this.subscriberFunction = subscriberFunction;
+  }
+
+  public [$$observable]() {
+    return this;
+  }
+
+  public subscribe(observer: Observer<T>): Subscription {
+    let subscriptionOrCleanupFunction = this.subscriberFunction(observer);
+
+    if (isSubscription(subscriptionOrCleanupFunction)) {
+      return subscriptionOrCleanupFunction;
+    } else {
+      return {
+        unsubscribe: subscriptionOrCleanupFunction,
+      };
+    }
+  }
+}
