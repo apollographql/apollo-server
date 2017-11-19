8b55c8119bcd12f4a8b4fcdb92611901bfccad07
Laurin Quast <laurinquast@googlemail.com>
2017-11-19 17:48:55 +0100

    🏗  Adds restify support

--- packages/apollo-server-restify/README.md
+++ packages/apollo-server-restify/README.md
@@ -38,7 +38,7 @@ server.use(restify.plugins.queryParser());
 server.post('/graphql', graphqlRestify(graphQLOptions));
 server.get('/graphql', graphqlRestify(graphQLOptions));
 
-server.get('/graphiql', graphiqlRestify({ endpointURL: '/graphql' }));
+server.get('/graphiql.*', graphiqlRestify({ endpointURL: '/graphql' }));
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 ```
--- packages/apollo-server-restify/src/restifyApollo.ts
+++ packages/apollo-server-restify/src/restifyApollo.ts
@@ -1,4 +1,6 @@
 import * as restify from 'restify';
+import * as path from 'path';
+import * as fs from 'fs';
 import * as url from 'url';
 import { GraphQLOptions, HttpQueryError, runHttpQuery } from 'apollo-server-core';
 import * as GraphiQL from 'apollo-server-module-graphiql';
@@ -29,28 +31,31 @@ export function graphqlRestify(options: GraphQLOptions | RestifyGraphQLOptionsFu
     runHttpQuery([req, res], {
       method: req.method,
       options: options,
-      query: req.method === 'POST' ? req.body : req.query,
-    }).then((gqlResponse) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.write(gqlResponse);
-      res.end();
-      next();
-    }, (error: HttpQueryError) => {
-      if ( 'HttpQueryError' !== error.name ) {
-        throw error;
-      }
+      query: req.method === 'POST' ? req.body : req.query
+    }).then(
+      gqlResponse => {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(gqlResponse);
+        res.end();
+        next();
+      },
+      (error: HttpQueryError) => {
+        if ('HttpQueryError' !== error.name) {
+          throw error;
+        }
 
-      if ( error.headers ) {
-        Object.keys(error.headers).forEach((header) => {
-          res.setHeader(header, error.headers[header]);
-        });
-      }
+        if (error.headers) {
+          Object.keys(error.headers).forEach(header => {
+            res.setHeader(header, error.headers[header]);
+          });
+        }
 
-      res.statusCode = error.statusCode;
-      res.write(error.message);
-      res.end();
-      next(false);
-    });
+        res.statusCode = error.statusCode;
+        res.write(error.message);
+        res.end();
+        next(false);
+      }
+    );
   };
 }
 
@@ -70,18 +75,41 @@ export interface RestifyGraphiQLOptionsFunction {
  */
 
 export function graphiqlRestify(options: GraphiQL.GraphiQLData | RestifyGraphiQLOptionsFunction) {
+  const publicPath = GraphiQL.resolveGraphiQLPublicPath();
+
   return (req: restify.Request, res: restify.Response, next: restify.Next) => {
-    const query = req.url && url.parse(req.url, true).query || {};
-    GraphiQL.resolveGraphiQLString(query, options, req).then(graphiqlString => {
-      res.setHeader('Content-Type', 'text/html');
-      res.write(graphiqlString);
-      res.end();
-      next();
-    }, error => {
-      res.statusCode = 500;
-      res.write(error.message);
-      res.end();
-      next(false);
-    });
+    const query = (req.url && url.parse(req.url, true).query) || {};
+
+    if (req.url.includes(`/assets`)) {
+      let error = null;
+      const fileName = path.basename(req.url);
+      const stream = fs.createReadStream(path.join(publicPath, fileName));
+
+      const extName = path.extname(fileName);
+      res.setHeader(`content-type`, extName === `.js` ? `application/javascript` : `text/css`);
+
+      stream.on(`error`, err => {
+        error = err;
+        res.status(404);
+        return res.send(`File ${req.url} not found.`);
+      });
+
+      return stream.pipe(res);
+    }
+
+    GraphiQL.resolveGraphiQLString(req.url, query, options, req).then(
+      graphiqlString => {
+        res.setHeader('Content-Type', 'text/html');
+        res.write(graphiqlString);
+        res.end();
+        next();
+      },
+      error => {
+        res.statusCode = 500;
+        res.write(error.message);
+        res.end();
+        next(false);
+      }
+    );
   };
 }
