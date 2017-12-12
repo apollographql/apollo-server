62c397eecc0150e6f94317ec99258b00018e22fa
Benedict Hobart <bennyhobart@users.noreply.github.com>
2017-12-12 18:09:01 +1100

    Fix shallow cloning of context when executing a batch request (#679)
    This fixes the default behavior for class-based contexts by also cloning the prototype, and it allows you to customize per-operation behavior by passing in a function as context.

--- packages/apollo-server-core/src/runHttpQuery.ts
+++ packages/apollo-server-core/src/runHttpQuery.ts
@@ -27,6 +27,10 @@ function isQueryOperation(query: DocumentNode, operationName: string) {
   return operationAST.operation === 'query';
 }
 
+function isFunction(arg: any): arg is Function {
+  return typeof arg === 'function';
+}
+
 export async function runHttpQuery(handlerArguments: Array<any>, request: HttpQueryRequest): Promise<string> {
   let isGetRequest: boolean = false;
   let optionsObject: GraphQLOptions;
@@ -97,19 +101,18 @@ export async function runHttpQuery(handlerArguments: Array<any>, request: HttpQu
         }
       }
 
-      // Shallow clone context for queries in batches. This allows
-      // users to distinguish multiple queries in the batch and to
-      // modify the context object without interfering with each other.
-      let context = optionsObject.context;
-      if (isBatch) {
-        context = Object.assign({},  context || {});
+      let context = optionsObject.context || {};
+      if (isFunction(context)) {
+        context = context();
+      } else if (isBatch) {
+        context = Object.assign(Object.create(Object.getPrototypeOf(context)), context);
       }
 
       let params = {
         schema: optionsObject.schema,
         query: query,
         variables: variables,
-        context: context,
+        context,
         rootValue: optionsObject.rootValue,
         operationName: operationName,
         logFunction: optionsObject.logFunction,
