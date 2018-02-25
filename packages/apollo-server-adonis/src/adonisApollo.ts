d3b58e72794813803b48306abfe2a12a37f17a65
Jelle Breuer <jelle@breuclan.nl>
2018-02-25 20:53:32 +0100

    fixed styling

--- packages/apollo-server-adonis/src/adonisApollo.ts
+++ packages/apollo-server-adonis/src/adonisApollo.ts
@@ -35,7 +35,7 @@ export function graphqlAdonis(
       query,
     }).then(
       gqlResponse => {
-        response.header('Content-type', 'application/json')
+        response.header('Content-type', 'application/json');
         response.json(gqlResponse);
       },
       (error: HttpQueryError) => {
