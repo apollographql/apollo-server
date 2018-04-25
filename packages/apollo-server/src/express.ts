8073474a35855a6c221ca9d81ae140ec7b64140d
Evans Hauser <evanshauser@gmail.com>
2018-04-25 13:40:20 -0700

    graphiql > gui and make content-type check only if necessary

--- packages/apollo-server/src/express.ts
+++ packages/apollo-server/src/express.ts
@@ -3,7 +3,7 @@ import * as cors from 'cors';
 import { json } from 'body-parser';
 import { createServer, Server as HttpServer } from 'http';
 import { graphqlExpress } from 'apollo-server-express';
-import graphiql from 'graphql-playground-middleware-express';
+import gui from 'graphql-playground-middleware-express';
 import * as accepts from 'accepts';
 
 import { ApolloServerBase } from './utils/ApolloServer';
@@ -22,21 +22,21 @@ export class ApolloServer extends ApolloServerBase<
     const path = config.path || '/graphql';
 
     app.use(path, cors(config.cors), json(), (req, res, next) => {
-      // only show graphiql if we want html for a GET
-      const accept = accepts(req);
-      const types = accept.types() as string[];
-      const isHTML =
-        types.find(
-          (x: string) => x === 'text/html' || x === 'application/json',
-        ) === 'text/html';
+      if (config.gui !== false && req.method === 'GET') {
+        //perform more expensive content-type check only if necessary
+        const accept = accepts(req);
+        const types = accept.types() as string[];
+        const prefersHTML =
+          types.find(
+            (x: string) => x === 'text/html' || x === 'application/json',
+          ) === 'text/html';
 
-      // make sure we check to see if graphiql should be on
-      // change opts.graphiql type to be boolean
-      if (config.graphiql !== false && req.method === 'GET' && isHTML) {
-        return graphiql({
-          endpoint: path,
-          subscriptionsEndpoint: config.subscriptions && path,
-        })(req, res, next);
+        if (prefersHTML) {
+          return gui({
+            endpoint: path,
+            subscriptionsEndpoint: config.subscriptions && path,
+          })(req, res, next);
+        }
       }
       return graphqlExpress(request)(req, res, next);
     });
