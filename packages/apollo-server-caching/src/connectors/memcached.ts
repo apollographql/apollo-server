c2373aa0610a047d13223176bebaac2330ebcf8d
clarencenpy <clarencenpy@gmail.com>
2018-06-14 17:14:09 -0700

    Merge branch 'server-2.0/caching-connectors' of /Users/clarencenpy/Apollo/apollo-server with conflicts.

--- packages/apollo-server-caching/src/connectors/memcached.ts
+++ packages/apollo-server-caching/src/connectors/memcached.ts
@@ -1,6 +1,7 @@
 import { KeyValueCache } from '../keyValueCache';
 import Memcached from 'memcached';
 import { promisify } from 'util';
+import uuid from 'uuid/v4';
 
 export default class MemcachedKeyValueCache implements KeyValueCache {
   readonly client;
@@ -8,6 +9,8 @@ export default class MemcachedKeyValueCache implements KeyValueCache {
     ttl: 300,
     tags: [],
   };
+
+  private versionSeed = Math.floor(Math.random() * 100 + 1);
 
   constructor(serverLocation: Memcached.Location, options?: Memcached.options) {
     this.client = new Memcached(serverLocation, options);
@@ -22,16 +25,43 @@ export default class MemcachedKeyValueCache implements KeyValueCache {
     data: string,
     options?: { ttl?: number; tags?: string[] },
   ): Promise<void> {
-    const { ttl } = Object.assign({}, this.defaultSetOptions, options);
-    await this.client.set(key, data, ttl);
+    const { ttl, tags } = Object.assign({}, this.defaultSetOptions, options);
+
+    try {
+      // generate version number
+      const version = uuid();
+
+      const setOperations = [{ key, data: version + data, ttl }];
+      await this.client.set(key, data, ttl);
+
+      // store each of the tags with corresponding version number
+      for (const tag of tags) {
+        setOperations.push({ tag });
+        await this;
+      }
+
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
   async get(key: string): Promise<string | undefined> {
-    return await this.client.get(key);
+    try {
+      const reply = await this.client.get(key);
+      return Promise.resolve(reply);
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
   async flush(): Promise<void> {
-    await this.client.flush();
+    try {
+      await this.client.flush();
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
   }
 
   async close(): Promise<void> {
