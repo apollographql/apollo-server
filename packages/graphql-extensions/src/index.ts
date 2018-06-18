9f49d140cf88a7e802bc2feae182d33cb108539c
Evans Hauser <evanshauser@gmail.com>
2018-06-18 16:57:30 -0700

    willSendResponse can return a result

--- packages/graphql-extensions/src/index.ts
+++ packages/graphql-extensions/src/index.ts
@@ -42,7 +42,10 @@ export class GraphQLExtension<TContext = any> {
     executionArgs: ExecutionArgs;
   }): EndHandler | void;
 
-  public willSendResponse?(o: { graphqlResponse: GraphQLResponse }): void;
+  public willSendResponse?(o: {
+    graphqlResponse: GraphQLResponse;
+    responseInit: ResponseInit;
+  }): { graphqlResponse?: GraphQLResponse; responseInit?: ResponseInit } | void;
 
   public willResolveField?(
     source: any,
@@ -93,12 +96,25 @@ export class GraphQLExtensionStack<TContext = any> {
     );
   }
 
-  public willSendResponse(o: { graphqlResponse: GraphQLResponse }): void {
-    this.extensions.forEach(extension => {
+  public willSendResponse(o: {
+    graphqlResponse: GraphQLResponse;
+    responseInit: ResponseInit;
+  }): { graphqlResponse: GraphQLResponse; responseInit: ResponseInit } {
+    return this.extensions.reduce((prev, extension) => {
       if (extension.willSendResponse) {
-        extension.willSendResponse(o);
+        const result = extension.willSendResponse(o);
+        if (result) {
+          if (result.graphqlResponse) {
+            prev.graphqlResponse = result.graphqlResponse;
+          }
+
+          if (result.responseInit) {
+            prev.responseInit = result.responseInit;
+          }
+        }
       }
-    });
+      return prev;
+    }, o);
   }
 
   public willResolveField(
