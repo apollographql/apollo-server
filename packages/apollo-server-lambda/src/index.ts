bb4f1c3a367b9dd0aab2db52829ba5f8828bc272
Alessio Dionisi <hello@adns.io>
2018-06-21 22:54:53 +0200

    Apollo Server 2.0: AWS Lambda Integration (#1188)
    * initial implementation of aws lambda integration for apollo server 2
    
    * ApolloServer class
    
    * package.json cleanup
    
    * parse json of body
    
    * createHandler function, complete README
    
    * cors options
    
    * update readme
    
    * lambdaApollo tests
    
    * fix tests

---
+++ packages/apollo-server-lambda/src/index.ts
@@ -0,0 +1,14 @@
+// Expose types which can be used by both middleware flavors.
+export { GraphQLOptions, gql } from 'apollo-server-core';
+
+export {
+  ApolloError,
+  toApolloError,
+  SyntaxError,
+  ValidationError,
+  AuthenticationError,
+  ForbiddenError,
+} from 'apollo-server-core';
+
+// ApolloServer integration
+export { ApolloServer, CreateHandlerOptions } from './ApolloServer';
