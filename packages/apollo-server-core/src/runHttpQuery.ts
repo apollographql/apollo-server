7ecb2fcfaaa7c0cf37d9c7d7ab25297c65b79fac
Martijn Walraven <martijn@martijnwalraven.com>
2018-06-27 11:40:46 +0200

    More imports from apollo-server-env

--- packages/apollo-server-core/src/runHttpQuery.ts
+++ packages/apollo-server-core/src/runHttpQuery.ts
@@ -6,6 +6,7 @@ import { CacheControlExtensionOptions } from 'apollo-cache-control';
 
 import { omit } from 'lodash';
 
+import { Request } from 'apollo-server-env';
 import { runQuery, QueryOptions } from './runQuery';
 import {
   default as GraphQLOptions,
