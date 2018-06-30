f2e4247a2e95839aa5e71f4653313166e4e64575
Hugh Willson <hugh@octonary.com>
2018-06-30 17:27:43 -0400

    Add playground support; Refactoring

--- packages/apollo-server-micro/src/ApolloServer.ts
+++ packages/apollo-server-micro/src/ApolloServer.ts
@@ -1,12 +1,18 @@
 import { ApolloServerBase, GraphQLOptions } from 'apollo-server-core';
 import { IncomingMessage, ServerResponse } from 'http';
 import { send } from 'micro';
+import {
+  renderPlaygroundPage,
+  MiddlewareOptions as PlaygroundMiddlewareOptions,
+} from 'graphql-playground-html';
+import { parseAll } from 'accept';
 
 import { graphqlMicro } from './microApollo';
 
 export interface ServerRegistration {
   disableHealthCheck?: boolean;
   onHealthCheck?: (req: IncomingMessage) => Promise<any>;
+  gui?: boolean | PlaygroundMiddlewareOptions;
 }
 
 export class ApolloServer extends ApolloServerBase {
@@ -23,37 +29,108 @@ export class ApolloServer extends ApolloServerBase {
   public graphqlHandler({
     disableHealthCheck,
     onHealthCheck,
+    gui,
   }: ServerRegistration = {}) {
+    return async (req, res) => {
+      (await this.handleHealthCheck({
+        req,
+        res,
+        disableHealthCheck,
+        onHealthCheck,
+      })) ||
+        this.handleGraphqlRequestsWithPlayground({ req, res, gui }) ||
+        (await this.handleGraphqlRequestsWithServer({ req, res }));
+    };
+  }
+
+  // If health checking is enabled trigger the `onHealthCheck`
+  // function, when the health check URL is requested.
+  private async handleHealthCheck({
+    req,
+    res,
+    disableHealthCheck,
+    onHealthCheck,
+  }: {
+    req: IncomingMessage;
+    res: ServerResponse;
+    disableHealthCheck?: boolean;
+    onHealthCheck?: (req: IncomingMessage) => Promise<any>;
+  }): boolean {
+    let handled = false;
+
+    if (
+      !disableHealthCheck &&
+      req.url === '/.well-known/apollo/server-health'
+    ) {
+      // Response follows
+      // https://tools.ietf.org/html/draft-inadarei-api-health-check-01
+      res.setHeader('Content-Type', 'application/health+json');
+
+      if (onHealthCheck) {
+        try {
+          await onHealthCheck(req);
+        } catch (error) {
+          send(res, 503, { status: 'fail' });
+        }
+      }
+
+      send(res, 200, { status: 'pass' });
+      handled = true;
+    }
+
+    return handled;
+  }
+
+  // If the `gui` option is set, register a `graphql-playground` instance
+  // (not available in production) that is then used to handle all
+  // incoming GraphQL requests.
+  private handleGraphqlRequestsWithPlayground({
+    req,
+    res,
+    gui,
+  }: {
+    req: IncomingMessage;
+    res: ServerResponse;
+    gui?: boolean | PlaygroundMiddlewareOptions;
+  }): boolean {
+    let handled = false;
+    const guiEnabled =
+      !!gui || (gui === undefined && process.env.NODE_ENV !== 'production');
+
+    if (guiEnabled && req.method === 'GET') {
+      const accept = parseAll(req.headers);
+      const types = accept.mediaTypes as string[];
+      const prefersHTML =
+        types.find(
+          (x: string) => x === 'text/html' || x === 'application/json',
+        ) === 'text/html';
+
+      if (prefersHTML) {
+        const middlewareOptions = {
+          endpoint: '/', // TODO
+          version: '1.7.0',
+          ...(typeof gui === 'boolean' ? {} : gui),
+        };
+        send(res, 200, renderPlaygroundPage(middlewareOptions));
+        handled = true;
+      }
+    }
+
+    return handled;
+  }
+
+  // Handle incoming GraphQL requests using Apollo Server.
+  private async handleGraphqlRequestsWithServer({
+    req,
+    res,
+  }: {
+    req: IncomingMessage;
+    res: ServerResponse;
+  }) {
     const graphqlHandler = graphqlMicro(
       this.createGraphQLServerOptions.bind(this),
     );
-    return async (req, res) => {
-      let statusCode: number = 200;
-      let data;
-
-      if (
-        !disableHealthCheck &&
-        req.url === '/.well-known/apollo/server-health'
-      ) {
-        let statusLabel: string = 'pass';
-
-        // Response follows
-        // https://tools.ietf.org/html/draft-inadarei-api-health-check-01
-        res.setHeader('Content-Type', 'application/health+json');
-
-        if (onHealthCheck) {
-          onHealthCheck(req).catch(() => {
-            statusCode = 503;
-            statusLabel = 'fail';
-          });
-        }
-
-        data = { status: statusLabel };
-      } else {
-        data = await graphqlHandler(req, res);
-      }
-
-      send(res, statusCode, data);
-    };
+    const responseData = await graphqlHandler(req, res);
+    send(res, 200, responseData);
   }
 }
