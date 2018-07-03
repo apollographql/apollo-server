19c8adf01975e9b9b95abb82398b43a9db1169db
Evans Hauser <evanshauser@gmail.com>
2018-07-03 15:08:42 -0700

    Import apollo-cache-control-js (#1294)
    * Initial commit
    
    * Add .npmignore to avoid ignoring lib when publishing
    
    * 0.0.2
    
    * Update graphql-extensions dependency to 0.0.2
    
    * 0.0.3
    
    * Reorganize code and add tests
    
    * 0.0.4
    
    * Add tests and reorganize code
    
    * 0.0.5
    
    * Update dependency to graphql-extensions 0.0.4
    
    * 0.0.6
    
    * Update graphql-extensions dependency and downgrade TS target
    
    * 0.0.7
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Update README
    
    * Add import instructions for TypeScript to README (#7)
    
    * Add cache hints when the directive is defined on interfaces (#10)
    
    * Increase version range for `graphql` peerDependency. (#12)
    
    * 0.0.8
    
    * Update dependencies
    
    * 0.0.9
    
    * [apollo-bot] Update the Issue/PR Templates with auto label (#13)
    
    * [apollo-bot] Update the Templates with docs label (#15)
    
    * Add cache hints to info.cacheControl (#16)
    
    This exposes the `cacheControl` hints on the schema, to the resolvers at runtime.
    
    * Update `graphql` peer dependency range to allow 0.13.x.
    
    * dev: Update TypeScript to latest version, v2.7.2.
    
    * dev: Update jest & dependencies to latest versions.
    
    * dev: Update type definitions for `graphql`, `node` and `jest`.
    
    * dev: Update `graphql` to latest version, v0.13.2.
    
    * 0.0.10
    
    * Add defaultMaxAge option
    
    This is an easy way to quickly say "I want everything in my schema to be cached
    for 5 seconds". You can override it with specific maxAges on specific fields or
    types.
    
    An upcoming release of apollo-server-* will allow you to specify options to
    cacheControl.
    
    * Add CircleCI config
    
    * Add partial CHANGELOG
    
    * 0.1.0
    
    * Honor overwrite default maxAge with maxAge=0 (Fixes #22) (#23)
    
    * 0.1.1
    
    * Allow default caching to apply to interfaces
    
    * remove unused files
    
    * update versions and use a-s style config
    
    * run prettier
    
    * fix tests and typing

---
+++ packages/apollo-cache-control-js/src/index.ts
@@ -0,0 +1,180 @@
+import {
+  DirectiveNode,
+  getNamedType,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLResolveInfo,
+  ResponsePath,
+  responsePathAsArray,
+} from 'graphql';
+
+import { GraphQLExtension } from 'graphql-extensions';
+
+export interface CacheControlFormat {
+  version: 1;
+  hints: ({ path: (string | number)[] } & CacheHint)[];
+}
+
+export interface CacheHint {
+  maxAge?: number;
+  scope?: CacheScope;
+}
+
+export enum CacheScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE',
+}
+
+export interface CacheControlExtensionOptions {
+  defaultMaxAge?: number;
+}
+
+declare module 'graphql/type/definition' {
+  interface GraphQLResolveInfo {
+    cacheControl: {
+      setCacheHint: (hint: CacheHint) => void;
+      cacheHint: CacheHint;
+    };
+  }
+}
+
+export class CacheControlExtension<TContext = any>
+  implements GraphQLExtension<TContext> {
+  private defaultMaxAge: number;
+
+  constructor(options: CacheControlExtensionOptions = {}) {
+    this.defaultMaxAge = options.defaultMaxAge || 0;
+  }
+
+  private hints: Map<ResponsePath, CacheHint> = new Map();
+
+  willResolveField(
+    _source: any,
+    _args: { [argName: string]: any },
+    _context: TContext,
+    info: GraphQLResolveInfo,
+  ) {
+    let hint: CacheHint = {};
+
+    // If this field's resolver returns an object or interface, look for hints
+    // on that return type.
+    const targetType = getNamedType(info.returnType);
+    if (
+      targetType instanceof GraphQLObjectType ||
+      targetType instanceof GraphQLInterfaceType
+    ) {
+      if (targetType.astNode) {
+        hint = mergeHints(
+          hint,
+          cacheHintFromDirectives(targetType.astNode.directives),
+        );
+      }
+    }
+
+    // If this field is a field on an object, look for hints on the field
+    // itself, taking precedence over previously calculated hints.
+    const parentType = info.parentType;
+    if (parentType instanceof GraphQLObjectType) {
+      const fieldDef = parentType.getFields()[info.fieldName];
+      if (fieldDef.astNode) {
+        hint = mergeHints(
+          hint,
+          cacheHintFromDirectives(fieldDef.astNode.directives),
+        );
+      }
+    }
+
+    // If this resolver returns an object and we haven't seen an explicit maxAge
+    // hint, set the maxAge to 0 (uncached) or the default if specified in the
+    // constructor.  (Non-object fields by default are assumed to inherit their
+    // cacheability from their parents.)
+    if (
+      (targetType instanceof GraphQLObjectType ||
+        targetType instanceof GraphQLInterfaceType) &&
+      hint.maxAge === undefined
+    ) {
+      hint.maxAge = this.defaultMaxAge;
+    }
+
+    if (hint.maxAge !== undefined || hint.scope !== undefined) {
+      this.addHint(info.path, hint);
+    }
+
+    info.cacheControl = {
+      setCacheHint: (hint: CacheHint) => {
+        this.addHint(info.path, hint);
+      },
+      cacheHint: hint,
+    };
+  }
+
+  addHint(path: ResponsePath, hint: CacheHint) {
+    const existingCacheHint = this.hints.get(path);
+    if (existingCacheHint) {
+      this.hints.set(path, mergeHints(existingCacheHint, hint));
+    } else {
+      this.hints.set(path, hint);
+    }
+  }
+
+  format(): [string, CacheControlFormat] {
+    return [
+      'cacheControl',
+      {
+        version: 1,
+        hints: Array.from(this.hints).map(([path, hint]) => ({
+          path: [...responsePathAsArray(path)],
+          ...hint,
+        })),
+      },
+    ];
+  }
+}
+
+function cacheHintFromDirectives(
+  directives: ReadonlyArray<DirectiveNode> | undefined,
+): CacheHint | undefined {
+  if (!directives) return undefined;
+
+  const cacheControlDirective = directives.find(
+    directive => directive.name.value === 'cacheControl',
+  );
+  if (!cacheControlDirective) return undefined;
+
+  if (!cacheControlDirective.arguments) return undefined;
+
+  const maxAgeArgument = cacheControlDirective.arguments.find(
+    argument => argument.name.value === 'maxAge',
+  );
+  const scopeArgument = cacheControlDirective.arguments.find(
+    argument => argument.name.value === 'scope',
+  );
+
+  // TODO: Add proper typechecking of arguments
+  return {
+    maxAge:
+      maxAgeArgument &&
+      maxAgeArgument.value &&
+      maxAgeArgument.value.kind === 'IntValue'
+        ? parseInt(maxAgeArgument.value.value)
+        : undefined,
+    scope:
+      scopeArgument &&
+      scopeArgument.value &&
+      scopeArgument.value.kind === 'EnumValue'
+        ? (scopeArgument.value.value as CacheScope)
+        : undefined,
+  };
+}
+
+function mergeHints(
+  hint: CacheHint,
+  otherHint: CacheHint | undefined,
+): CacheHint {
+  if (!otherHint) return hint;
+
+  return {
+    maxAge: otherHint.maxAge !== undefined ? otherHint.maxAge : hint.maxAge,
+    scope: otherHint.scope || hint.scope,
+  };
+}
