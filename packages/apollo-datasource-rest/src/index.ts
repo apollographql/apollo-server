f8feca870aa977b01661560bf5144815f426fc94
Martijn Walraven <martijn@martijnwalraven.com>
2018-07-09 16:51:30 -0700

    Move lru-cache dependency to apollo-server-caching

--- packages/apollo-datasource-rest/src/index.ts
+++ packages/apollo-datasource-rest/src/index.ts
@@ -1,3 +1,2 @@
 export { RESTDataSource, RequestOptions } from './RESTDataSource';
 export { HTTPCache } from './HTTPCache';
-export { KeyValueCache, InMemoryLRUCache } from 'apollo-server-caching';
