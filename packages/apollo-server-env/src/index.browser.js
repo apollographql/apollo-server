c723274091b717381b356c1fd63061333ff26e6a
Martijn Walraven <martijn@martijnwalraven.com>
2018-07-09 08:30:35 -0700

    Make apollo-server-env work with Cloudflare Workers and fly.io

--- packages/apollo-server-env/src/index.browser.js
+++ packages/apollo-server-env/src/index.browser.js
@@ -1,2 +1,44 @@
-const { fetch, Request, Response, Headers, URL, URLSearchParams } = global;
+if (!global) {
+  global = self;
+}
+
+let { fetch, Request, Response, Headers, URL, URLSearchParams } = global;
+fetch = fetch.bind(global);
 export { fetch, Request, Response, Headers, URL, URLSearchParams };
+
+if (!global.process) {
+  global.process = {};
+}
+
+if (!global.process.env) {
+  global.process.env = {
+    NODE_ENV: 'production',
+  };
+}
+
+if (!global.process.version) {
+  global.process.version = '';
+}
+
+if (!global.process.hrtime) {
+  // Adapted from https://github.com/kumavis/browser-process-hrtime
+  global.process.hrtime = function hrtime(previousTimestamp) {
+    var clocktime = Date.now() * 1e-3;
+    var seconds = Math.floor(clocktime);
+    var nanoseconds = Math.floor((clocktime % 1) * 1e9);
+    if (previousTimestamp) {
+      seconds = seconds - previousTimestamp[0];
+      nanoseconds = nanoseconds - previousTimestamp[1];
+      if (nanoseconds < 0) {
+        seconds--;
+        nanoseconds += 1e9;
+      }
+    }
+    return [seconds, nanoseconds];
+  };
+}
+
+if (!global.os) {
+  // FIXME: Add some sensible values
+  global.os = {};
+}
