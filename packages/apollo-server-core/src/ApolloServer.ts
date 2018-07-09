5a0dc16fddcac5555a001463738b855891f9935f
clarencenpy <clarencenpy@gmail.com>
2018-07-08 21:01:46 -0700

    Integrate @defer support into `apollo-server-core`

--- packages/apollo-server-core/src/ApolloServer.ts
+++ packages/apollo-server-core/src/ApolloServer.ts
@@ -1,7 +1,6 @@
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import { Server as HttpServer } from 'http';
 import {
-  execute,
   GraphQLSchema,
   subscribe,
   ExecutionResult,
@@ -10,6 +9,7 @@ import {
   ValidationContext,
   FieldDefinitionNode,
 } from 'graphql';
+import { execute } from './execute';
 import { GraphQLExtension } from 'graphql-extensions';
 import { EngineReportingAgent } from 'apollo-engine-reporting';
 import { InMemoryLRUCache } from 'apollo-server-caching';
@@ -19,6 +19,7 @@ import { GraphQLUpload } from 'apollo-upload-server';
 import {
   SubscriptionServer,
   ExecutionParams,
+  ServerOptions,
 } from 'subscriptions-transport-ws';
 
 import { formatApolloErrors } from 'apollo-server-errors';
@@ -313,7 +314,7 @@ export class ApolloServerBase {
           return { ...connection, context };
         },
         keepAlive,
-      },
+      } as ServerOptions, //TODO: Resolve type errors
       {
         server,
         path,
