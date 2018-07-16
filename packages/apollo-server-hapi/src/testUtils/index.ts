510f4c154aadd0bb9b5a1dbd91a1f9eaf8589d93
Jake <dawkinsjh@gmail.com>
2018-07-16 11:32:34 -0400

    added hapi testutil

---
+++ packages/apollo-server-hapi/src/testUtils/index.ts
@@ -0,0 +1,33 @@
+import { HttpLink } from 'apollo-link-http';
+import { Server } from 'hapi';
+import * as http from 'http';
+import fetch from 'node-fetch';
+import { AddressInfo } from 'net';
+import { execute } from 'apollo-link';
+export { toPromise } from 'apollo-link';
+
+import { ApolloServer } from '../';
+
+export const startTestServer = async (server: ApolloServer) => {
+  const app = new Server({ host: 'localhost', port: 0 });
+
+  server.applyMiddleware({ app });
+  await app.start();
+
+  const port = app.info.port;
+
+  const link = new HttpLink({
+    uri: `http://localhost:${port}/graphql`,
+    fetch,
+  });
+
+  const executeOperation = ({
+    query,
+    variables = {},
+  }: {
+    query: any;
+    variables: Record<string, any>;
+  }) => execute(link, { query, variables });
+
+  return { link, stop: () => app.stop(), graphql: executeOperation };
+};
