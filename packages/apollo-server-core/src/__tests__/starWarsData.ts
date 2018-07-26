f5d9f2b99c933894e77cf217c580db1093d01a2e
clarencenpy <clarencenpy@gmail.com>
2018-07-25 19:18:23 -0700

    Clean up and move `graphql.js` attribution into LICENSE

--- packages/apollo-server-core/src/__tests__/starWarsData.ts
+++ packages/apollo-server-core/src/__tests__/starWarsData.ts
@@ -1,12 +1,4 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-/**
  * This defines a basic set of data for our Star Wars Schema.
  *
  * This data is hard coded for the sake of the demo, but you could imagine
