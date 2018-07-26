996a3f72d9ad940003c5d605ea43ec6b953b41c1
clarencenpy <clarencenpy@gmail.com>
2018-07-25 19:18:23 -0700

    Clean up and move `graphql.js` attribution into LICENSE

--- packages/apollo-server-core/src/__tests__/graphql.ts
+++ packages/apollo-server-core/src/__tests__/graphql.ts
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
- */
-
 import { validateSchema } from 'graphql/type/validate';
 import { parse } from 'graphql/language/parser';
 import { validate } from 'graphql/validation/validate';
