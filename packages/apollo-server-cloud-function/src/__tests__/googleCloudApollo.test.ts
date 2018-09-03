41b8cd2f8cc7a92b79e7ea8484701d4b07facd86
Marcel Miranda <m@reaktivo.com>
2018-09-03 18:03:22 +0200

    Removed bogus route check, since GCF does handle different routes, added body parsing by default

--- packages/apollo-server-cloud-function/src/__tests__/googleCloudApollo.test.ts
+++ packages/apollo-server-cloud-function/src/__tests__/googleCloudApollo.test.ts
@@ -5,24 +5,17 @@ import testSuite, {
 } from 'apollo-server-integration-testsuite';
 import { Config } from 'apollo-server-core';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 const createCloudFunction = (options: CreateAppOptions = {}) => {
   const handler = new ApolloServer(
     (options.graphqlOptions as Config) || { schema: Schema },
   ).createHandler();
 
-  const bogus = (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-  ) => {
-    // return 404 if path is /bogus-route to pass the test, lambda doesn't have paths
-    req.url.includes('/bogus-route') ? res.status(404).end() : next();
-  };
-  // We use express to convert the raw Node request and response
-  // into a Google Cloud Functions compatible request and response
+  // We use Express to simulate the Google Cloud
+  // Function like environment
   const app = express();
-  app.use(bogus);
+  app.use(bodyParser.json());
   app.use(handler);
   return app;
 };
