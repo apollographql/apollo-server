23903a10137aca8f3d7acd9537a061e844b2d2e2
Jesse Rosenberger <git@jro.cc>
2018-10-09 01:56:52 +0300

    Switch to the `prod` operation manifest bucket.

--- packages/apollo-server-plugin-operation-registry/src/common.ts
+++ packages/apollo-server-plugin-operation-registry/src/common.ts
@@ -6,7 +6,7 @@ const envOverrideOperationManifest = 'APOLLO_OPERATION_MANIFEST_BASE_URL';
 const urlOperationManifestBase: string = ((): string => {
   const desiredUrl =
     process.env[envOverrideOperationManifest] ||
-    'https://storage.googleapis.com/engine-schema-reg-abernix-query-reg/dev/';
+    'https://storage.googleapis.com/engine-op-manifest-storage-prod/';
 
   // Make sure it has NO trailing slash.
   return desiredUrl.replace(/\/$/, '');
