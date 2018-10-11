68b2c691f84391c360af0648fef4626144aae3ac
Martijn Walraven <martijn@martijnwalraven.com>
2018-10-11 23:09:22 +0200

    Move test `tsconfig.json` files under `__tests__` to support editors
    The TypeScript language server looks for `tsconfig.json` and doesn't take project references into account when resolving the config for a file.
    
    See https://github.com/Microsoft/TypeScript/issues/27372.

--- jest.config.base.js
+++ jest.config.base.js
@@ -20,7 +20,7 @@ module.exports = {
     clearMocks: true,
     globals: {
       "ts-jest": {
-        tsConfig: "<rootDir>/tsconfig.test.json",
+        tsConfig: "<rootDir>/src/__tests__/tsconfig.json",
         diagnostics: false
       }
     }
