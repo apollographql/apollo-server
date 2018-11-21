832440b0b74e4cb1d92723e08f92ed88b4723181
Jesse Rosenberger <git@jro.cc>
2018-11-21 16:46:55 +0200

    Instantiate `lru-cache` class using `new` in `InMemoryLRUCache`.
    This is mandated by `lru-cache` v5 and surfaced the CircleCI failures on #2004:
    
    https://circleci.com/gh/apollographql/apollo-server/22537
    
    Luckily, this is a private implementation detail of Apollo Server's
    `InMemoryLRUCache` so no additional changes should be necessary and we should
    be able to update to `lru-cache` 5.0.0 in a semver minor respectful way.

--- packages/apollo-server-caching/src/InMemoryLRUCache.ts
+++ packages/apollo-server-caching/src/InMemoryLRUCache.ts
@@ -6,7 +6,7 @@ export class InMemoryLRUCache implements KeyValueCache {
 
   // FIXME: Define reasonable default max size of the cache
   constructor({ maxSize = Infinity }: { maxSize?: number } = {}) {
-    this.store = LRU({
+    this.store = new LRU({
       max: maxSize,
       length: item => item.length,
     });
