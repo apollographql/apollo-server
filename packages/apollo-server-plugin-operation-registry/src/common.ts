c67a233d8a564513dc3cbf9ac55baa3006ceddae
Jesse Rosenberger <git@jro.cc>
2018-12-03 16:48:42 +0200

    Stop hiding object and list literals during operation signature generation.

--- packages/apollo-server-plugin-operation-registry/src/common.ts
+++ packages/apollo-server-plugin-operation-registry/src/common.ts
@@ -1,29 +1,54 @@
 export const pluginName: string = require('../package.json').name;
 import { createHash } from 'crypto';
-import { parse } from 'graphql/language';
+import { parse } from 'graphql/language/parser';
+import { visit } from 'graphql/language/visitor';
 import {
-  hideLiterals,
-  printWithReducedWhitespace,
-  sortAST,
-} from 'apollo-engine-reporting';
+  DocumentNode,
+  IntValueNode,
+  FloatValueNode,
+  StringValueNode,
+} from 'graphql/language/ast';
+import { printWithReducedWhitespace, sortAST } from 'apollo-engine-reporting';
 
 export const envOverrideOperationManifest =
   'APOLLO_OPERATION_MANIFEST_BASE_URL';
+
+// DUPLICATED FROM https://github.com/apollographql/apollo-tooling/blob/d7429b80/packages/apollo/src/commands/client/push.ts#L29-L48
+// In the same spirit as the similarly named `hideLiterals` function from the
+// `apollo-engine-reporting/src/signature.ts` module, we'll do an AST visit
+// to redact literals.  Developers are strongly encouraged to use the
+// `variables` aspect of the which would avoid these being explicitly
+// present in the operation manifest at all.  The primary area of concern here
+// is to avoid sending in-lined literals which might contain sensitive
+// information (e.g. API keys, etc.).
+export function hideCertainLiterals(ast: DocumentNode): DocumentNode {
+  return visit(ast, {
+    IntValue(node: IntValueNode): IntValueNode {
+      return { ...node, value: '0' };
+    },
+    FloatValue(node: FloatValueNode): FloatValueNode {
+      return { ...node, value: '0' };
+    },
+    StringValue(node: StringValueNode): StringValueNode {
+      return { ...node, value: '', block: false };
+    },
+  });
+}
 
 // XXX This function is not currently invoked when the hashed APQ value
 // (`extensions.persistedQuery.sha256Hash`) is set, but would
 // be called if we're provided a (string) `query` (so when APQ is a miss,
 // or not enabled at all).
 //
-// Since the `hideLiterals` and `sortAST` methods both require a
+// Since the `hideCertainLiterals` and `sortAST` methods both require a
 // `DocumentNode`, we'll have to call a `parse` here on the `query` string
 // and make the necessary transformations.
 //
-// Lastly, we will remove the `hideLiterals` portion of this once the
+// Lastly, we will remove the `hideCertainLiterals` portion of this once the
 // `apollo` CLI properly warns about including/using string literals.
 function formatOperationForHashing(operation: string): string {
   return printWithReducedWhitespace(
-    sortAST(hideLiterals(parse(operation))),
+    sortAST(hideCertainLiterals(parse(operation))),
   ).trim();
 }
 
