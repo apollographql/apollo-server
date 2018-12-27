043c6ee1f274b452bfac0c1238bbd11741e69100
Laurens Lavaert <llavaert@gmail.com>
2018-12-27 09:19:09 +0100

    feat: Allow context to be overwritten
    In the current implementation you would have to set up a seperate Apollo Server instance with a context function or object that suits the needs of your testing suite.
    
    By allowing to adjust this directly in the createTestClient it takes away this verbosity.

--- packages/apollo-server-testing/src/createTestClient.ts
+++ packages/apollo-server-testing/src/createTestClient.ts
@@ -3,19 +3,23 @@ import { print, DocumentNode } from 'graphql';
 
 type StringOrAst = string | DocumentNode;
 
+type Context<T = any> = T;
+type ContextFunction<T = any> = (context: Context<T>) => Promise<Context<T>>;
+
+type Options = { context?: Context | ContextFunction };
 // A query must not come with a mutation (and vice versa).
 type Query = { query: StringOrAst; mutation?: undefined };
 type Mutation = { mutation: StringOrAst; query?: undefined };
 
-export default (server: ApolloServerBase) => {
+export default (server: ApolloServerBase, options: Options) => {
   const executeOperation = server.executeOperation.bind(server);
+  // Override default context behaviour if context option is provided.
+  if (options.context) server.requestOptions.context = options.context;
   const test = ({ query, mutation, ...args }: Query | Mutation) => {
     const operation = query || mutation;
 
     if ((!query && !mutation) || (query && mutation)) {
-      throw new Error(
-        'Either `query` or `mutation` must be passed, but not both.',
-      );
+      throw new Error('Either `query` or `mutation` must be passed, but not both.');
     }
 
     return executeOperation({
