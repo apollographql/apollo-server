04542814731acbf99781320e7edd64d3cf24cb27
Trevor Blades <tdblades@gmail.com>
2019-01-28 17:04:23 -0800

    Build docs with gatsby

---
+++ docs/gatsby-config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: 'gatsby-theme-apollo',
+      options: {
+        root: __dirname,
+        subtitle: 'Apollo Server',
+        description: 'A guide to using Apollo Server.',
+        basePath: '/platform',
+        sidebarCategories: {
+          null: [
+            'index',
+            'getting-started',
+            'whats-new'
+          ],
+          Essentials: [
+            'essentials/schema',
+            'essentials/server',
+            'essentials/data'
+          ],
+          Features: [
+            'features/mocking',
+            'features/errors',
+            'features/data-sources',
+            'features/subscriptions',
+            'features/metrics',
+            'features/graphql-playground',
+            'features/scalars-enums',
+            'features/unions-interfaces',
+            'features/directives',
+            'features/creating-directives',
+            'features/authentication',
+            'features/testing'
+          ],
+          // 'Schema stitching': [
+          //   'features/schema-stitching',
+          //   'features/remote-schemas',
+          //   'features/schema-delegation',
+          //   'features/schema-transforms',
+          // ],
+          Deployment: [
+            // 'deployment/index',
+            'deployment/heroku',
+            'deployment/lambda',
+            'deployment/now'
+          ],
+          'API Reference': [
+            'api/apollo-server',
+            'api/graphql-tools'
+          ],
+          Migration: [
+            'migration-two-dot',
+            'migration-engine',
+            'migration-file-uploads'
+          ]
+        }
+      }
+    }
+  ]
+};
