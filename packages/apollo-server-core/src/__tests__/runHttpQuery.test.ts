98e5a65c9ce8cb5ce0752e993621af945d1084c7
Paolo Ragone <pragone@gmail.com>
2019-02-04 22:31:37 +1300

    Fixes #1709. Based on the PR from @MrLoh but ensuring linting is OK and doing the rest of the homework so it can be merged

--- packages/apollo-server-core/src/__tests__/runHttpQuery.test.ts
+++ packages/apollo-server-core/src/__tests__/runHttpQuery.test.ts
@@ -4,6 +4,7 @@ import MockReq = require('mock-req');
 import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
 
 import { runHttpQuery, HttpQueryError } from '../runHttpQuery';
+import { AuthenticationError, ForbiddenError, ValidationError } from 'apollo-server-core';
 
 const queryType = new GraphQLObjectType({
   name: 'QueryType',
@@ -43,6 +44,83 @@ describe('runHttpQuery', () => {
       return runHttpQuery([], noQueryRequest).catch((err: HttpQueryError) => {
         expect(err.statusCode).toEqual(400);
         expect(err.message).toEqual('Must provide query string.');
+      });
+    });
+  });
+  describe('error handling in context function', () => {
+    const mockQueryRequest = {
+      method: 'GET',
+      query: {
+        query: '{ testString }',
+      },
+      options: {
+        schema,
+      },
+      request: new MockReq(),
+    };
+
+    it('raises a 401 error if an AuthenticationError is thrown', () => {
+      const noQueryRequest = Object.assign({}, mockQueryRequest, {
+        options: {
+          ...mockQueryRequest.options,
+          context: () => {
+            throw new AuthenticationError('This is the error');
+          }
+        }
+      });
+
+      expect.assertions(2);
+      return runHttpQuery([], noQueryRequest).catch((err: HttpQueryError) => {
+        expect(err.statusCode).toEqual(401);
+        expect(err.message.trim()).toEqual("{\"errors\":[{\"message\":\"Context creation failed: This is the error\",\"extensions\":{\"code\":\"UNAUTHENTICATED\"}}]}");
+      });
+    });
+    it('raises a 403 error if a ForbiddenError is thrown', () => {
+      const noQueryRequest = Object.assign({}, mockQueryRequest, {
+        options: {
+          ...mockQueryRequest.options,
+          context: () => {
+            throw new ForbiddenError('This is the error');
+          }
+        }
+      });
+
+      expect.assertions(2);
+      return runHttpQuery([], noQueryRequest).catch((err: HttpQueryError) => {
+        expect(err.statusCode).toEqual(403);
+        expect(err.message.trim()).toEqual("{\"errors\":[{\"message\":\"Context creation failed: This is the error\",\"extensions\":{\"code\":\"FORBIDDEN\"}}]}");
+      });
+    });
+    it('raises a 400 error if any other GraphQL error is thrown', () => {
+      const noQueryRequest = Object.assign({}, mockQueryRequest, {
+        options: {
+          ...mockQueryRequest.options,
+          context: () => {
+            throw new ValidationError('This is the error');
+          }
+        }
+      });
+
+      expect.assertions(2);
+      return runHttpQuery([], noQueryRequest).catch((err: HttpQueryError) => {
+        expect(err.statusCode).toEqual(400);
+        expect(err.message.trim()).toEqual("{\"errors\":[{\"message\":\"Context creation failed: This is the error\",\"extensions\":{\"code\":\"GRAPHQL_VALIDATION_FAILED\"}}]}");
+      });
+    });
+    it('raises a 500 error if any other error is thrown', () => {
+      const noQueryRequest = Object.assign({}, mockQueryRequest, {
+        options: {
+          ...mockQueryRequest.options,
+          context: () => {
+            throw new Error('This is the error');
+          }
+        }
+      });
+
+      expect.assertions(2);
+      return runHttpQuery([], noQueryRequest).catch((err: HttpQueryError) => {
+        expect(err.statusCode).toEqual(500);
+        expect(err.message.trim()).toEqual("{\"errors\":[{\"message\":\"Context creation failed: This is the error\",\"extensions\":{\"code\":\"INTERNAL_SERVER_ERROR\"}}]}");
       });
     });
   });
