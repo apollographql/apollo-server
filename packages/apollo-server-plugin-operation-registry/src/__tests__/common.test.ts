f1f49777bb6a30b5ab1fb580a5c2b503902bc0c2
Trevor Scheer <trevor@apollographql.com>
2019-03-12 18:22:55 -0700

    Update operation registry plugin to use manifest v2
    * Use shared functions from apollo-graphql for
      normalization and hashing
    * Append the v2/ prefix to the operation manifest URL

--- packages/apollo-server-plugin-operation-registry/src/__tests__/common.test.ts
+++ packages/apollo-server-plugin-operation-registry/src/__tests__/common.test.ts
@@ -4,4 +4,10 @@ describe('common', () => {
   it('uses the correct cache prefix', () => {
     expect(common.getStoreKey('abc123')).toStrictEqual('apq:abc123');
   });
+
+  it('uses the v2 manifest prefix', () => {
+    expect(
+      common.getOperationManifestUrl('aServiceId', 'aSchemaHash'),
+    ).toStrictEqual('v2/aServiceId/aSchemaHash');
+  });
 });
--- packages/apollo-server-plugin-operation-registry/src/common.ts
+++ packages/apollo-server-plugin-operation-registry/src/common.ts
@@ -1,58 +1,8 @@
 export const pluginName: string = require('../package.json').name;
 import { createHash } from 'crypto';
-import { visit } from 'graphql/language/visitor';
-import {
-  DocumentNode,
-  IntValueNode,
-  FloatValueNode,
-  StringValueNode,
-} from 'graphql/language/ast';
-import {
-  printWithReducedWhitespace,
-  sortAST,
-} from 'apollo-graphql/lib/transforms';
 
 export const envOverrideOperationManifest =
   'APOLLO_OPERATION_MANIFEST_BASE_URL';
-
-// DUPLICATED FROM https://github.com/apollographql/apollo-tooling/blob/d7429b80/packages/apollo/src/commands/client/push.ts#L29-L48
-// In the same spirit as the similarly named `hideLiterals` function from the
-// `apollo-engine-reporting/src/signature.ts` module, we'll do an AST visit
-// to redact literals.  Developers are strongly encouraged to use the
-// `variables` aspect of the which would avoid these being explicitly
-// present in the operation manifest at all.  The primary area of concern here
-// is to avoid sending in-lined literals which might contain sensitive
-// information (e.g. API keys, etc.).
-export function hideCertainLiterals(ast: DocumentNode): DocumentNode {
-  return visit(ast, {
-    IntValue(node: IntValueNode): IntValueNode {
-      return { ...node, value: '0' };
-    },
-    FloatValue(node: FloatValueNode): FloatValueNode {
-      return { ...node, value: '0' };
-    },
-    StringValue(node: StringValueNode): StringValueNode {
-      return { ...node, value: '', block: false };
-    },
-  });
-}
-
-// XXX This function is not currently invoked when the hashed APQ value
-// (`extensions.persistedQuery.sha256Hash`) is set, but would
-// be called if we're provided a (string) `query` (so when APQ is a miss,
-// or not enabled at all).
-//
-// Since the `hideCertainLiterals` and `sortAST` methods both require a
-// `DocumentNode`, we'll have to call a `parse` here on the `query` string
-// and make the necessary transformations.
-//
-// Lastly, we will remove the `hideCertainLiterals` portion of this once the
-// `apollo` CLI properly warns about including/using string literals.
-function formatDocumentForHashing(document: DocumentNode): string {
-  return printWithReducedWhitespace(
-    sortAST(hideCertainLiterals(document)),
-  ).trim();
-}
 
 // Generate and cache our desired operation manifest URL.
 const urlOperationManifestBase: string = ((): string => {
@@ -72,17 +22,13 @@ export function generateServiceIdHash(serviceId: string): string {
     .digest('hex');
 }
 
-export function generateNormalizedDocumentHash(document: DocumentNode): string {
-  return createHash('sha256')
-    .update(formatDocumentForHashing(document))
-    .digest('hex');
-}
-
 export function getOperationManifestUrl(
   hashedServiceId: string,
   schemaHash: string,
 ): string {
-  return [urlOperationManifestBase, hashedServiceId, schemaHash].join('/');
+  return [urlOperationManifestBase, 'v2', hashedServiceId, schemaHash].join(
+    '/',
+  );
 }
 
 export function hashForLogging(hash: string): string {
