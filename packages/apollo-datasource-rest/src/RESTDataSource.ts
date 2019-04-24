95e1b4c8bd3016eb00c17abc50c469ac9aef3a76
Doug McCall <doug.mccall@fireeye.com>
2019-04-24 18:37:23 -0400

    Fix RESTDataSource 204 response handling

--- packages/apollo-datasource-rest/src/RESTDataSource.ts
+++ packages/apollo-datasource-rest/src/RESTDataSource.ts
@@ -103,10 +103,12 @@ export abstract class RESTDataSource<TContext = any> extends DataSource {
 
   protected parseBody(response: Response): Promise<object | string> {
     const contentType = response.headers.get('Content-Type');
+    const contentLength = response.headers.get('Content-Length');
     if (
       contentType &&
       (contentType.startsWith('application/json') ||
-        contentType.startsWith('application/hal+json'))
+        contentType.startsWith('application/hal+json')) &&
+      parseInt(contentLength as string, 10) > 0
     ) {
       return response.json();
     } else {
