5056a746b1f4a6a366e72e510ac9d83ffc0a9b1c
Chang Wang <garnwraly@gmail.com>
2019-06-01 12:29:43 -0400

    Add tests to ensure isDirectiveDefiend works for strings

--- packages/apollo-server-core/src/__tests__/isDirectiveDefined.ts
+++ packages/apollo-server-core/src/__tests__/isDirectiveDefined.ts
@@ -2,43 +2,46 @@ import { gql } from '../';
 import { isDirectiveDefined } from '../utils/isDirectiveDefined';
 
 describe('isDirectiveDefined', () => {
-  it('returns false when a directive is not defined', () => {
-    expect(
-      isDirectiveDefined(
-        [
-          gql`
-            type Query {
-              hello: String
-            }
-          `,
-        ],
-        'cacheControl',
-      ),
-    ).toBe(false);
+  const noCacheControl = `
+    type Query {
+      hello: String
+    }
+  `;
+  const hasCacheControl = `
+    type Query {
+      hello: String
+    }
+
+    enum CacheControlScope {
+      PUBLIC
+      PRIVATE
+    }
+
+    directive @cacheControl(
+      maxAge: Int
+      scope: CacheControlScope
+    ) on FIELD_DEFINITION | OBJECT | INTERFACE
+  `;
+
+  describe('When passed an array of DocumentNode', () => {
+    it('returns false when a directive is not defined', () => {
+      expect(isDirectiveDefined([gql(noCacheControl)], 'cacheControl')).toBe(
+        false,
+      );
+    });
+    it('returns true when a directive is defined', () => {
+      expect(isDirectiveDefined([gql(hasCacheControl)], 'cacheControl')).toBe(
+        true,
+      );
+    });
   });
 
-  it('returns true when a directive is defined', () => {
-    expect(
-      isDirectiveDefined(
-        [
-          gql`
-            type Query {
-              hello: String
-            }
-
-            enum CacheControlScope {
-              PUBLIC
-              PRIVATE
-            }
-
-            directive @cacheControl(
-              maxAge: Int
-              scope: CacheControlScope
-            ) on FIELD_DEFINITION | OBJECT | INTERFACE
-          `,
-        ],
-        'cacheControl',
-      ),
-    ).toBe(true);
+  describe('When passed a string', () => {
+    it('returns false when a directive is not defined', () => {
+      expect(isDirectiveDefined(noCacheControl, 'cacheControl')).toBe(false);
+    });
+    it('returns true when a directive is defined', () => {
+      expect(isDirectiveDefined(hasCacheControl, 'cacheControl')).toBe(true);
+    });
   });
 });
