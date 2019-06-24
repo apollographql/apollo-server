7a724419eb7db64518e6859ee432ebb5f2aed434
Jake <dawkinsjh@gmail.com>
2019-06-24 16:01:32 -0400

    Add validation rule extensions

--- packages/apollo-federation/src/composition/__tests__/composeAndValidate.test.ts
+++ packages/apollo-federation/src/composition/__tests__/composeAndValidate.test.ts
@@ -111,12 +111,52 @@ it('composes and validates all (24) permutations without error', () => {
     }
 
     expect({ warnings, errors }).toMatchInlineSnapshot(`
-                  Object {
-                    "errors": Array [],
-                    "warnings": Array [],
-                  }
-            `);
+                              Object {
+                                "errors": Array [],
+                                "warnings": Array [],
+                              }
+                    `);
   });
+});
+
+it('errors when a type extension has no base', () => {
+  const serviceA = {
+    typeDefs: gql`
+      schema {
+        query: MyRoot
+      }
+
+      type MyRoot {
+        products: [Product]!
+      }
+
+      type Product @key(fields: "sku") {
+        sku: String!
+        upc: String!
+      }
+    `,
+    name: 'serviceA',
+  };
+
+  const serviceB = {
+    typeDefs: gql`
+      extend type Location {
+        id: ID
+      }
+    `,
+    name: 'serviceB',
+  };
+
+  const { schema, errors } = composeAndValidate([serviceA, serviceB]);
+  expect(errors).toHaveLength(1);
+  expect(errors).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "code": "EXTENSION_WITH_NO_BASE",
+        "message": "[serviceB] Location -> \`Location\` is an extension type, but \`Location\` is not defined in any service",
+      },
+    ]
+  `);
 });
 
 it('treats types with @extends as type extensions', () => {
@@ -149,12 +189,12 @@ it('treats types with @extends as type extensions', () => {
 
   const product = schema.getType('Product') as GraphQLObjectType;
   expect(product).toMatchInlineSnapshot(`
-            type Product {
-              sku: String!
-              upc: String!
-              price: Int!
-            }
-      `);
+                    type Product {
+                      sku: String!
+                      upc: String!
+                      price: Int!
+                    }
+          `);
 });
 
 it('errors on invalid usages of default operation names', () => {
@@ -196,13 +236,13 @@ it('errors on invalid usages of default operation names', () => {
 
   const { errors } = composeAndValidate([serviceA, serviceB]);
   expect(errors).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "code": "ROOT_QUERY_USED",
-        "message": "[serviceA] Query -> Found invalid use of default root operation name \`Query\`. \`Query\` is disallowed when \`Schema.query\` is set to a type other than \`Query\`.",
-      },
-    ]
-  `);
+            Array [
+              Object {
+                "code": "ROOT_QUERY_USED",
+                "message": "[serviceA] Query -> Found invalid use of default root operation name \`Query\`. \`Query\` is disallowed when \`Schema.query\` is set to a type other than \`Query\`.",
+              },
+            ]
+      `);
 });
 
 it.todo('errors on duplicate types where there is a mismatch of field types');
