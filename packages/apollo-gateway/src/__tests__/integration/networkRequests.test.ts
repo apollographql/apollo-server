950d8e40c22f388bb742ebfc228a91c18ffa3d2a
Jackson Kearl <jackson.kearl@apollographql.com>
2019-06-25 14:25:37 -0700

    Make mock process more explicit

--- packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
@@ -1,5 +1,6 @@
 import nock from 'nock';
 import { ApolloGateway } from '../..';
+import createSHA from '../../utilities/createSHA';
 
 import {
   mockGetRawPartialSchema,
@@ -11,10 +12,23 @@ import {
 } from './nockMocks';
 
 it('Queries remote endpoints for their SDLs', async () => {
-  mockLocalhostSDLQuery();
+  let url = 'localhost:4001';
+  let sdl = `extend type Query {
+      me: User
+    everyone: [User]
+  }
+
+  type User @key(fields: "id") {
+      id: ID!
+    name: String
+    username: String
+  }
+  `;
+
+  mockLocalhostSDLQuery({ url, sdl });
 
   let gateway = new ApolloGateway({
-    serviceList: [{ name: 'accounts', url: 'http://localhost:4001/graphql' }],
+    serviceList: [{ name: 'accounts', url: 'http://localhost:4001/' }],
   });
   await gateway.load();
   expect(nock.isDone()).toBeTruthy();
@@ -22,15 +36,54 @@ it('Queries remote endpoints for their SDLs', async () => {
 
 // This test is maybe a bit terrible, but IDK a better way to mock all the requests
 it('Extracts service definitions from remote storage', async () => {
-  mockFetchStorageSecret();
-  mockGetCompositionConfigLink();
-  mockGetCompositionConfigs();
-  mockGetImplementingServices();
-  mockGetRawPartialSchema();
+  let apiKey = 'service:jacksons-service:AABBCCDDEEFFGG';
+  let apiKeyHash = createSHA('sha512')
+    .update(apiKey)
+    .digest('hex');
 
-  let gateway = new ApolloGateway({
-    apiKey: 'service:mdg-private-6077:EgWp3sa01FhGuMJSKIfMVQ',
+  let storageSecret = 'secret';
+  let serviceName = 'jacksons-service';
+  let implementingServicePath = 'path-to-implementing-service-definition.json';
+  let partialSchemaPath = 'path-to-accounts-partial-schema.json';
+  let federatedServiceName = 'accounts';
+  let federatedServiceURL = 'http://localhost:4001';
+  let federatedServiceSchema = `
+        extend type Query {
+        me: User
+        everyone: [User]
+      }
+
+      type User @key(fields: "id") {
+        id: ID!
+        name: String
+        username: String
+      }`;
+
+  mockFetchStorageSecret({ apiKeyHash, storageSecret, serviceName });
+
+  mockGetCompositionConfigLink(storageSecret);
+
+  mockGetCompositionConfigs({
+    storageSecret,
+    implementingServicePath,
+    federatedServiceName,
   });
+
+  mockGetImplementingServices({
+    storageSecret,
+    implementingServicePath,
+    partialSchemaPath,
+    federatedServiceName,
+    federatedServiceURL,
+  });
+
+  mockGetRawPartialSchema({
+    storageSecret,
+    partialSchemaPath,
+    federatedServiceSchema,
+  });
+
+  let gateway = new ApolloGateway({ apiKey });
   await gateway.load();
   expect(nock.isDone()).toBeTruthy();
 });
