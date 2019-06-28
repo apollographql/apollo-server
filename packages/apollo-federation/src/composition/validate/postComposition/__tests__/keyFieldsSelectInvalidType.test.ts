ed709957d27523bd7c0dae86c8bc4120d5f70d49
Evans Hauser <evanshauser@gmail.com>
2019-06-27 17:40:59 -0700

    add test for @key referencing list

--- packages/apollo-federation/src/composition/validate/postComposition/__tests__/keyFieldsSelectInvalidType.test.ts
+++ packages/apollo-federation/src/composition/validate/postComposition/__tests__/keyFieldsSelectInvalidType.test.ts
@@ -72,13 +72,13 @@ describe('keyFieldsSelectInvalidType', () => {
 
     const warnings = validateKeyFieldsSelectInvalidType(schema);
     expect(warnings).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "code": "KEY_FIELDS_SELECT_INVALID_TYPE",
-          "message": "[serviceA] Product -> A @key selects Product.featuredItem, which is an interface type. Keys cannot select interfaces.",
-        },
-      ]
-    `);
+            Array [
+              Object {
+                "code": "KEY_FIELDS_SELECT_INVALID_TYPE",
+                "message": "[serviceA] Product -> A @key selects Product.featuredItem, which is an interface type. Keys cannot select interfaces.",
+              },
+            ]
+        `);
   });
 
   it('warns if @key references fields of a union type', () => {
@@ -109,12 +109,45 @@ describe('keyFieldsSelectInvalidType', () => {
 
     const warnings = validateKeyFieldsSelectInvalidType(schema);
     expect(warnings).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "code": "KEY_FIELDS_SELECT_INVALID_TYPE",
-          "message": "[serviceA] Product -> A @key selects Product.price, which is a union type. Keys cannot select union types.",
-        },
-      ]
-    `);
+            Array [
+              Object {
+                "code": "KEY_FIELDS_SELECT_INVALID_TYPE",
+                "message": "[serviceA] Product -> A @key selects Product.price, which is a union type. Keys cannot select union types.",
+              },
+            ]
+        `);
+  });
+
+  it('warns if @key references fields of a list type', () => {
+    const serviceA = {
+      typeDefs: gql`
+        type Product @key(fields: "price") {
+          sku: String!
+          price: [Numeric]!
+        }
+
+        type Numeric {
+          euro: Int
+          usd: Int
+        }
+      `,
+      name: 'serviceA',
+    };
+
+    const serviceB = {
+      typeDefs: gql`
+        extend type Product {
+          sku: String! @external
+          name: String!
+        }
+      `,
+      name: 'serviceB',
+    };
+
+    const { schema, errors } = composeServices([serviceA, serviceB]);
+    expect(errors).toHaveLength(0);
+
+    const warnings = validateKeyFieldsSelectInvalidType(schema);
+    expect(warnings).toMatchInlineSnapshot(`Array []`);
   });
 });
