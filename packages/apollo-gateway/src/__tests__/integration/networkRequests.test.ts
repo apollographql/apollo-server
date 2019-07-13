6b07d0680fbfe00b54778b72ca26939b2070ee6d
Jackson Kearl <jackson.kearl@apollographql.com>
2019-07-12 17:38:53 -0700

    Add back my tests

--- packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
@@ -146,129 +146,129 @@ it('Rollsback to a previous schema when triggered', async () => {
         username: String
       }`;
 
-  // // Init
-  // mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
-  //   200,
-  //   `"${storageSecret}"`,
-  // );
+  // Init
+  mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
+    200,
+    `"${storageSecret}"`,
+  );
 
-  // mockGetCompositionConfigLink(storageSecret).reply(200, {
-  //   configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
-  // });
+  mockGetCompositionConfigLink(storageSecret).reply(200, {
+    configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
+  });
 
-  // mockGetCompositionConfigs({
-  //   storageSecret,
-  // }).reply(200, {
-  //   implementingServiceLocations: [
-  //     {
-  //       name: federatedServiceName,
-  //       path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath1}.json`,
-  //     },
-  //   ],
-  // });
+  mockGetCompositionConfigs({
+    storageSecret,
+  }).reply(200, {
+    implementingServiceLocations: [
+      {
+        name: federatedServiceName,
+        path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath1}.json`,
+      },
+    ],
+  });
 
-  // mockGetImplementingServices({
-  //   storageSecret,
-  //   implementingServicePath: implementingServicePath1,
-  //   federatedServiceName,
-  // }).reply(200, {
-  //   name: federatedServiceName,
-  //   partialSchemaPath: `${storageSecret}/current/raw-partial-schemas/${partialSchemaPath1}`,
-  //   url: federatedServiceURL1,
-  // });
+  mockGetImplementingServices({
+    storageSecret,
+    implementingServicePath: implementingServicePath1,
+    federatedServiceName,
+  }).reply(200, {
+    name: federatedServiceName,
+    partialSchemaPath: `${storageSecret}/current/raw-partial-schemas/${partialSchemaPath1}`,
+    url: federatedServiceURL1,
+  });
 
-  // mockGetRawPartialSchema({
-  //   storageSecret,
-  //   partialSchemaPath: partialSchemaPath1,
-  // }).reply(200, federatedServiceSchema1);
+  mockGetRawPartialSchema({
+    storageSecret,
+    partialSchemaPath: partialSchemaPath1,
+  }).reply(200, federatedServiceSchema1);
 
-  // // Update 1
-  // mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
-  //   200,
-  //   `"${storageSecret}"`,
-  // );
+  // Update 1
+  mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
+    200,
+    `"${storageSecret}"`,
+  );
 
-  // mockGetCompositionConfigLink(storageSecret).reply(200, {
-  //   configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
-  // });
+  mockGetCompositionConfigLink(storageSecret).reply(200, {
+    configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
+  });
 
-  // mockGetCompositionConfigs({
-  //   storageSecret,
-  // }).reply(200, {
-  //   implementingServiceLocations: [
-  //     {
-  //       name: federatedServiceName,
-  //       path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath2}.json`,
-  //     },
-  //   ],
-  // });
+  mockGetCompositionConfigs({
+    storageSecret,
+  }).reply(200, {
+    implementingServiceLocations: [
+      {
+        name: federatedServiceName,
+        path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath2}.json`,
+      },
+    ],
+  });
 
-  // mockGetImplementingServices({
-  //   storageSecret,
-  //   implementingServicePath: implementingServicePath2,
-  //   federatedServiceName,
-  // }).reply(200, {
-  //   name: federatedServiceName,
-  //   partialSchemaPath: `${storageSecret}/current/raw-partial-schemas/${partialSchemaPath2}`,
-  //   url: federatedServiceURL2,
-  // });
+  mockGetImplementingServices({
+    storageSecret,
+    implementingServicePath: implementingServicePath2,
+    federatedServiceName,
+  }).reply(200, {
+    name: federatedServiceName,
+    partialSchemaPath: `${storageSecret}/current/raw-partial-schemas/${partialSchemaPath2}`,
+    url: federatedServiceURL2,
+  });
 
-  // mockGetRawPartialSchema({
-  //   storageSecret,
-  //   partialSchemaPath: partialSchemaPath2,
-  // }).reply(200, federatedServiceSchema2);
+  mockGetRawPartialSchema({
+    storageSecret,
+    partialSchemaPath: partialSchemaPath2,
+  }).reply(200, federatedServiceSchema2);
 
-  // // Rollback
-  // mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
-  //   200,
-  //   `"${storageSecret}"`,
-  // );
+  // Rollback
+  mockFetchStorageSecret({ apiKeyHash, serviceName }).reply(
+    200,
+    `"${storageSecret}"`,
+  );
 
-  // mockGetCompositionConfigLink(storageSecret).reply(200, {
-  //   configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
-  // });
+  mockGetCompositionConfigLink(storageSecret).reply(200, {
+    configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
+  });
 
-  // mockGetCompositionConfigs({
-  //   storageSecret,
-  // }).reply(200, {
-  //   implementingServiceLocations: [
-  //     {
-  //       name: federatedServiceName,
-  //       path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath1}.json`,
-  //     },
-  //   ],
-  // });
+  mockGetCompositionConfigs({
+    storageSecret,
+  }).reply(200, {
+    implementingServiceLocations: [
+      {
+        name: federatedServiceName,
+        path: `${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath1}.json`,
+      },
+    ],
+  });
 
-  // mockGetImplementingServices({
-  //   storageSecret,
-  //   implementingServicePath: implementingServicePath1,
-  //   federatedServiceName,
-  // }).reply(304);
+  mockGetImplementingServices({
+    storageSecret,
+    implementingServicePath: implementingServicePath1,
+    federatedServiceName,
+  }).reply(304);
 
-  // mockGetRawPartialSchema({
-  //   storageSecret,
-  //   partialSchemaPath: partialSchemaPath1,
-  // }).reply(304);
+  mockGetRawPartialSchema({
+    storageSecret,
+    partialSchemaPath: partialSchemaPath1,
+  }).reply(304);
 
-  // jest.useFakeTimers();
+  jest.useFakeTimers();
 
-  // const onChange = jest.fn();
-  // const gateway = new ApolloGateway();
-  // await gateway.load({ engine: { apiKeyHash, graphId: serviceName } });
-  // gateway.onSchemaChange(onChange);
-  // jest.runOnlyPendingTimers();
+  const onChange = jest.fn();
+  const gateway = new ApolloGateway();
+  await gateway.load({ engine: { apiKeyHash, graphId: serviceName } });
+  gateway.onSchemaChange(onChange);
+  jest.runOnlyPendingTimers();
 
-  // jest.useRealTimers();
-  // await new Promise(resolve => setTimeout(resolve, 100));
-  // jest.useFakeTimers();
+  jest.useRealTimers();
+  await new Promise(resolve => setTimeout(resolve, 100));
+  jest.useFakeTimers();
 
-  // expect(onChange.mock.calls.length).toBe(1);
+  expect(onChange.mock.calls.length).toBe(1);
 
-  // jest.runOnlyPendingTimers();
+  jest.runOnlyPendingTimers();
 
-  // jest.useRealTimers();
-  // await new Promise(resolve => setTimeout(resolve, 100));
-  // jest.useFakeTimers();
+  jest.useRealTimers();
+  await new Promise(resolve => setTimeout(resolve, 100));
+  jest.useFakeTimers();
 
-  // expect(onChange.mock.calls.length).toBe(2);
+  expect(onChange.mock.calls.length).toBe(2);
 });
