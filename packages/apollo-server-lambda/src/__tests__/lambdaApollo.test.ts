82da3c238c842dbda33ec81ff1f45b91594a8c33
David Sanders <dsanders11@ucsbalum.com>
2019-08-01 06:14:30 -0700

    Add test coverage

--- packages/apollo-server-lambda/src/__tests__/lambdaApollo.test.ts
+++ packages/apollo-server-lambda/src/__tests__/lambdaApollo.test.ts
@@ -51,6 +51,48 @@ const createLambda = (options: CreateAppOptions = {}) => {
   };
 };
 
+const createLambdaAsync = (options: CreateAppOptions = {}) => {
+  const server = new ApolloServer(
+    (options.graphqlOptions as Config) || { schema: Schema },
+  );
+
+  const handler = server.createHandler({ async: true });
+
+  return (req: IncomingMessage, res: ServerResponse) => {
+    // return 404 if path is /bogus-route to pass the test, lambda doesn't have paths
+    if (req.url.includes('/bogus-route')) {
+      res.statusCode = 404;
+      return res.end();
+    }
+
+    let body = '';
+    req.on('data', chunk => (body += chunk));
+    req.on('end', async () => {
+      const urlObject = url.parse(req.url, true);
+      const event = {
+        httpMethod: req.method,
+        body: body,
+        path: req.url,
+        queryStringParameters: urlObject.query,
+        requestContext: {
+          path: urlObject.pathname,
+        },
+        headers: req.headers,
+      };
+      const result = await handler(event as any, {} as any);
+      res.statusCode = result.statusCode;
+      for (let key in result.headers) {
+        if (result.headers.hasOwnProperty(key)) {
+          res.setHeader(key, result.headers[key]);
+        }
+      }
+      res.write(result.body);
+      res.end();
+    });
+  };
+};
+
 describe('integration:Lambda', () => {
   testSuite(createLambda);
+  testSuite(createLambdaAsync);
 });
