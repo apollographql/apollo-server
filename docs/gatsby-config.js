554c0174b887e49b2f8a9f9a33a47846029f3f46
Trevor Blades <tdblades@gmail.com>
2019-08-27 14:08:11 -0700

    Update theme to v2

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,9 +1,12 @@
+const themeOptions = require('gatsby-theme-apollo-docs/theme-options');
+
 module.exports = {
   pathPrefix: '/docs/apollo-server',
-  __experimentalThemes: [
+  plugins: [
     {
       resolve: 'gatsby-theme-apollo-docs',
       options: {
+        ...themeOptions,
         root: __dirname,
         subtitle: 'Apollo Server',
         description: 'A guide to using Apollo Server',
