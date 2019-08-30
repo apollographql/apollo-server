f0da5f8b783fcf1f38c8c1377084d1d920a8aa87
Jesse Rosenberger <git@jro.cc>
2019-08-30 16:48:33 +0300

    Fix peculiar indentation of `jest.config.js` file.

--- v3/jest.config.js
+++ v3/jest.config.js
@@ -2,10 +2,10 @@ const { defaults } = require("jest-config");
 
 module.exports = {
   testEnvironment: "node",
-    preset: "ts-jest",
-    testPathIgnorePatterns: [
-      "<rootDir>/node_modules/",
-      "<rootDir>/dist/"
-    ],
-    clearMocks: true,
+  preset: "ts-jest",
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/"
+  ],
+  clearMocks: true,
 };
