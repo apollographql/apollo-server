49cd7332edc5d36f05b4aabd67d123cab688383e
Jesse Rosenberger <git@jro.cc>
2019-09-14 14:11:04 +0300

    Remove deprecated `registerServer` function. (#3290)
    This cleans up some pre-release cruft which is no longer necessary.
    
    The `registerServer` method was the pre-release name for today's
    `applyMiddleware` method in early (again, pre-release) versions of Apollo
    Server.
    
    It's been `throw`ing since the first _stable_ release of Apollo Server so
    this isn't a breaking change.

--- packages/apollo-server-express/src/index.ts
+++ packages/apollo-server-express/src/index.ts
@@ -24,7 +24,6 @@ export * from 'graphql-subscriptions';
 // ApolloServer integration.
 export {
   ApolloServer,
-  registerServer,
   ServerRegistration,
   ApolloServerExpressConfig,
 } from './ApolloServer';
