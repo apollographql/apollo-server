2e7a33023e7fdc1ab73349f75416fa9fe075f40c
Trevor Scheer <trevor@apollographql.com>
2019-11-08 11:53:41 -0800

    Keep using core-js polyfills previously provided by apollo-env

--- packages/apollo-federation/jest.config.js
+++ packages/apollo-federation/jest.config.js
@@ -1,3 +1,8 @@
 const config = require('../../jest.config.base');
 
-module.exports = config;
+module.exports = Object.assign(Object.create(null), config, {
+  setupFiles: [
+    'core-js/features/array/flat',
+    'core-js/features/array/flat-map',
+  ],
+});
