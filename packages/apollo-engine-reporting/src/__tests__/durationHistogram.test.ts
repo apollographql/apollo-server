e7b975154d28a5a42779254f540ec4a39c8a6bd0
Joshua Segaran <joshua.segaran@apollographql.com>
2020-02-01 18:24:59 -0800

    Add more tests and add durationNS to bucket

--- packages/apollo-engine-reporting/src/__tests__/durationHistogram.test.ts
+++ packages/apollo-engine-reporting/src/__tests__/durationHistogram.test.ts
@@ -1,32 +1,81 @@
-import {DurationHistogram} from "../durationHistogram";
+import { DurationHistogram } from "../durationHistogram";
+
+function assertToArrayHelper(expected: number[], buckets: number[]) {
+  expect(new DurationHistogram(buckets).toArray()).toEqual(expected);
+}
 
 describe("Duration histogram tests", () => {
-  it('generateEmptyHistogram', () => {
+  it("generateEmptyHistogram", () => {
     let emptyDurationHistogram = new DurationHistogram();
-    expect([]).toEqual(emptyDurationHistogram.serialize());
+    expect([]).toEqual(emptyDurationHistogram.toArray());
   });
 
-  it('nonEmptyHistogram', () => {
+  it("nonEmptyHistogram", () => {
     let nonEmptyDurationHistogram = new DurationHistogram();
-    nonEmptyDurationHistogram.increment(100);
-    expect([-100, 1]).toEqual(nonEmptyDurationHistogram.serialize());
+    nonEmptyDurationHistogram.incrementBucket(100);
+    expect([-100, 1]).toEqual(nonEmptyDurationHistogram.toArray());
 
-    nonEmptyDurationHistogram.increment(102);
-    expect([-100, 1, 0, 1]).toEqual(nonEmptyDurationHistogram.serialize());
+    nonEmptyDurationHistogram.incrementBucket(102);
+    expect([-100, 1, 0, 1]).toEqual(nonEmptyDurationHistogram.toArray());
 
-    nonEmptyDurationHistogram.increment(382);
-    expect([-100, 1, 0, 1, -279, 1]).toEqual(nonEmptyDurationHistogram.serialize());
+    nonEmptyDurationHistogram.incrementBucket(382);
+    expect([-100, 1, 0, 1, -279, 1]).toEqual(nonEmptyDurationHistogram.toArray());
   });
 
-  it('combineHistogram', () => {
+  it("testToArray", () => {
+    assertToArrayHelper([], []);
+    assertToArrayHelper([], [0]);
+    assertToArrayHelper([], [0, 0, 0, 0]);
+    assertToArrayHelper([1], [1]);
+    assertToArrayHelper([100_000_000_000], [100_000_000_000]);
+    assertToArrayHelper([1, 0, 5], [1, 0, 5]);
+    assertToArrayHelper([1, -2, 5], [1, 0, 0, 5]);
+    assertToArrayHelper([0, 5], [0, 5]);
+    assertToArrayHelper([0, 1, -2, 2, -3, 3, -2, 4, 0, 5], [0, 1, 0, 0, 2, 0, 0, 0, 3, 0, 0, 4, 0, 5, 0]);
+    assertToArrayHelper([-2, 5], [0, 0, 5]);
+    assertToArrayHelper([-3, 5], [0, 0, 0, 5]);
+    assertToArrayHelper([-2, 5, -3, 10], [0, 0, 5, 0, 0, 0, 10]);
+  });
+
+  it("combineHistogram", () => {
     let firstHistogram = new DurationHistogram();
-    firstHistogram.increment(20);
+    firstHistogram.incrementBucket(20);
     let secondHistogram = new DurationHistogram();
-    secondHistogram.increment(40);
+    secondHistogram.incrementBucket(40);
 
     firstHistogram.combine(secondHistogram);
 
-    expect([-20, 1, -19, 1]).toEqual(firstHistogram.serialize());
+    expect([-20, 1, -19, 1]).toEqual(firstHistogram.toArray());
   });
 
+  it("bucketZeroToOne", () => {
+    expect(DurationHistogram.durationToBucket(-1)).toEqual(0);
+    expect(DurationHistogram.durationToBucket(0)).toEqual(0);
+    expect(DurationHistogram.durationToBucket(1)).toEqual(0);
+    expect(DurationHistogram.durationToBucket(999)).toEqual(0);
+    expect(DurationHistogram.durationToBucket(1000)).toEqual(0);
+    expect(DurationHistogram.durationToBucket(1001)).toEqual(1);
+  });
+
+  it("bucketOneToTwo", () => {
+    expect(DurationHistogram.durationToBucket(1100)).toEqual(1);
+    expect(DurationHistogram.durationToBucket(1101)).toEqual(2);
+  });
+
+  it("bucketToThreshold", () => {
+    expect(DurationHistogram.durationToBucket(10000)).toEqual(25);
+    expect(DurationHistogram.durationToBucket(10834)).toEqual(25);
+    expect(DurationHistogram.durationToBucket(10835)).toEqual(26);
+  });
+
+  it("bucketForCommonTimes", () => {
+    expect(DurationHistogram.durationToBucket(1e5)).toEqual(49);
+    expect(DurationHistogram.durationToBucket(1e6)).toEqual(73);
+    expect(DurationHistogram.durationToBucket(1e9)).toEqual(145);
+  });
+
+  it("testLastBucket", () => {
+    // Test an absurdly large number gets stuck in the last bucket
+    expect(DurationHistogram.durationToBucket(1e64)).toEqual(383);
+  });
 });
--- packages/apollo-engine-reporting/src/durationHistogram.ts
+++ packages/apollo-engine-reporting/src/durationHistogram.ts
@@ -1,9 +1,11 @@
 export class DurationHistogram {
   private readonly buckets: number[];
+  public static readonly BUCKET_COUNT = 384;
+  public static readonly EXPONENT_LOG = Math.log(1.1);
 
   public toArray(): number[] {
     let bufferedZeroes = 0;
-    let outputArray = Array<number>();
+    const outputArray: number[] = [];
 
     for (const value of this.buckets) {
       if (value === 0) {
@@ -21,21 +23,35 @@ export class DurationHistogram {
     return outputArray;
   }
 
-  public increment(bucket: number) {
+  static durationToBucket(durationNs: number): number {
+    const log = Math.log(durationNs / 1000.0);
+    const unboundedBucket = Math.ceil(log / DurationHistogram.EXPONENT_LOG);
+
+    // Comapre <= 0 to catch -0
+    return (unboundedBucket <= 0 || Number.isNaN(unboundedBucket)) ?
+      0 :
+      (unboundedBucket >= DurationHistogram.BUCKET_COUNT) ? DurationHistogram.BUCKET_COUNT - 1 : unboundedBucket;
+  }
+
+
+  public incrementDuration(durationNs: number) {
+    this.incrementBucket(DurationHistogram.durationToBucket(durationNs));
+  }
+
+  public incrementBucket(bucket: number) {
     this.buckets[bucket]++;
   }
 
   public combine(otherHistogram: DurationHistogram) {
-    for (let i = 0; i < otherHistogram.buckets.length; i++)  {
-      this.buckets[i] += otherHistogram.buckets[i]
+    for (let i = 0; i < otherHistogram.buckets.length; i++) {
+      this.buckets[i] += otherHistogram.buckets[i];
     }
   }
 
   constructor(buckets?: number[]) {
+    this.buckets = Array<number>(DurationHistogram.BUCKET_COUNT).fill(0);
     if (buckets) {
-      this.buckets = Object.assign([], buckets);
-    } else {
-     this.buckets = Array<number>(383).fill(0);
+      buckets.forEach((val, index) => this.buckets[index] = val);
     }
   }
 }
