4155e73ff8541b12944947236cb854d258e08746
Trevor Scheer <trevor.scheer@gmail.com>
2020-02-26 17:12:35 -0800

    Cleanup networkRequests tests
    * Centralize mocking fixtures and reuse them
    * Simplify mock request signatures for more easily read and written tests
    * Create nock/nockSuccess pairs for the most common use case and flexibility
    
    cc @abernix

--- packages/apollo-gateway/src/__tests__/integration/nockMocks.ts
+++ packages/apollo-gateway/src/__tests__/integration/nockMocks.ts
@@ -15,55 +15,95 @@ export const mockLocalhostSDLQuery = ({ url }: { url: string }) =>
     query: 'query GetServiceDefinition { _service { sdl } }',
   });
 
-export const mockFetchStorageSecret = ({
-  apiKeyHash,
-  serviceName,
-}: {
-  apiKeyHash: string;
-  serviceName: string;
-}) =>
+export const graphId = 'federated-service';
+export const apiKeyHash = 'dd55a79d467976346d229a7b12b673ce';
+const storageSecret = 'my-storage-secret';
+const accountsService = 'accounts';
+
+export const mockStorageSecret = () =>
   gcsNock('https://storage.googleapis.com:443').get(
-    `/engine-partial-schema-prod/${serviceName}/storage-secret/${apiKeyHash}.json`,
+    `/engine-partial-schema-prod/${graphId}/storage-secret/${apiKeyHash}.json`,
   );
 
+export const mockStorageSecretSuccess = () =>
+  gcsNock('https://storage.googleapis.com:443')
+    .get(
+      `/engine-partial-schema-prod/${graphId}/storage-secret/${apiKeyHash}.json`,
+    )
+    .reply(200, `"${storageSecret}"`);
+
 // get composition config link, using received storage secret
-export const mockGetCompositionConfigLink = (storageSecret: string) =>
+export const mockCompositionConfigLink = () =>
   gcsNock('https://storage.googleapis.com:443').get(
     `/engine-partial-schema-prod/${storageSecret}/current/v1/composition-config-link`,
   );
 
+export const mockCompositionConfigLinkSuccess = () =>
+  mockCompositionConfigLink().reply(200, {
+    configPath: `${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
+  });
+
 // get composition configs, using received composition config link
-export const mockGetCompositionConfigs = ({
-  storageSecret,
-}: {
-  storageSecret: string;
-}) =>
+export const mockCompositionConfigs = () =>
   gcsNock('https://storage.googleapis.com:443').get(
     `/engine-partial-schema-prod/${storageSecret}/current/v1/composition-configs/composition-config-path.json`,
   );
 
+export const mockCompositionConfigsSuccess = (
+  implementingServicePaths: string[],
+) =>
+  mockCompositionConfigs().reply(200, {
+    implementingServiceLocations: implementingServicePaths.map(servicePath => ({
+      name: accountsService,
+      path: `${storageSecret}/current/v1/implementing-services/${accountsService}/${servicePath}`,
+    })),
+  });
+
 // get implementing service reference, using received composition-config
-export const mockGetImplementingServices = ({
-  storageSecret,
+export const mockImplementingServices = ({
   implementingServicePath,
-  federatedServiceName,
 }: {
-  storageSecret: string;
   implementingServicePath: string;
-  federatedServiceName: string;
 }) =>
   gcsNock('https://storage.googleapis.com:443').get(
-    `/engine-partial-schema-prod/${storageSecret}/current/v1/implementing-services/${federatedServiceName}/${implementingServicePath}`,
+    `/engine-partial-schema-prod/${storageSecret}/current/v1/implementing-services/${accountsService}/${implementingServicePath}`,
   );
 
+export const mockImplementingServicesSuccess = ({
+  implementingServicePath,
+  partialSchemaPath,
+  federatedServiceURL,
+}: {
+  implementingServicePath: string;
+  partialSchemaPath: string;
+  federatedServiceURL: string;
+}) =>
+  mockImplementingServices({
+    implementingServicePath,
+  }).reply(200, {
+    name: accountsService,
+    partialSchemaPath: `${storageSecret}/current/raw-partial-schemas/${partialSchemaPath}`,
+    url: federatedServiceURL,
+  });
+
 // get raw-partial-schema, using received composition-config
-export const mockGetRawPartialSchema = ({
-  storageSecret,
+export const mockRawPartialSchema = ({
   partialSchemaPath,
 }: {
-  storageSecret: string;
   partialSchemaPath: string;
 }) =>
   gcsNock('https://storage.googleapis.com:443').get(
     `/engine-partial-schema-prod/${storageSecret}/current/raw-partial-schemas/${partialSchemaPath}`,
+  );
+
+export const mockRawPartialSchemaSuccess = ({
+  partialSchemaPath,
+  federatedServiceSchema,
+}: {
+  partialSchemaPath: string;
+  federatedServiceSchema: string;
+}) =>
+  mockRawPartialSchema({ partialSchemaPath }).reply(
+    200,
+    federatedServiceSchema,
   );
