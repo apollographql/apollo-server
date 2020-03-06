aaa16a25115f1795e46281b3a098b13570c0ef1d
Jesse Rosenberger <git@jro.cc>
2020-03-06 15:54:22 -0800

    gateway: Warn when both local and remote configurations are enabled.
    To help address the configuration when a local configuration is provided and
    one is also available in the cloud, warn the user of the dual-configuration
    to avoid what is a common unintended mode of operation.

--- packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/networkRequests.test.ts
@@ -11,7 +11,10 @@ import {
   graphId,
   mockImplementingServices,
   mockRawPartialSchema,
+  mockCompositionConfigLink,
 } from './nockMocks';
+
+import loadServicesFromStorage = require("../../loadServicesFromStorage");
 
 // This is a nice DX hack for GraphQL code highlighting and formatting within the file.
 // Anything wrapped within the gql tag within this file is just a string, not an AST.
@@ -92,6 +95,59 @@ it('Extracts service definitions from remote storage', async () => {
 
   await gateway.load({ engine: { apiKeyHash, graphId } });
   expect(gateway.schema!.getType('User')!.description).toBe('This is my User');
+});
+
+it.each([
+  ['warned', 'present'],
+  ['not warned', 'absent'],
+])('conflicting configurations are %s about when %s', async (_word, mode) => {
+  const isConflict = mode === 'present';
+  const spyConsoleWarn = jest.spyOn(console, 'warn');
+  let blockerResolve: () => void;
+  const blocker = new Promise(resolve => (blockerResolve = resolve));
+  const original = loadServicesFromStorage.getServiceDefinitionsFromStorage;
+  const spyGetServiceDefinitionsFromStorage = jest
+    .spyOn(loadServicesFromStorage, 'getServiceDefinitionsFromStorage')
+    .mockImplementationOnce(async (...args) => {
+      try {
+        return await original(...args);
+      } catch (e) {
+        throw e;
+      } finally {
+        setImmediate(blockerResolve);
+      }
+    });
+
+  mockStorageSecretSuccess();
+  if (isConflict) {
+    mockCompositionConfigLinkSuccess();
+    mockCompositionConfigsSuccess([service.implementingServicePath]);
+    mockImplementingServicesSuccess(service);
+    mockRawPartialSchemaSuccess(service);
+  } else {
+    mockCompositionConfigLink().reply(403);
+  }
+
+  mockLocalhostSDLQuery({ url: service.federatedServiceURL }).reply(200, {
+    data: { _service: { sdl: service.federatedServiceSchema } },
+  });
+
+  const gateway = new ApolloGateway({
+    serviceList: [
+      { name: 'accounts', url: `${service.federatedServiceURL}/graphql` },
+    ],
+  });
+
+  await gateway.load({ engine: { apiKeyHash, graphId } });
+  await blocker; // Wait for the definitions to be "fetched".
+
+  (isConflict
+    ? expect(spyConsoleWarn)
+    : expect(spyConsoleWarn).not
+  ).toHaveBeenCalledWith(expect.stringMatching(
+    /A local gateway service list is overriding an Apollo Graph Manager managed configuration/));
+  spyGetServiceDefinitionsFromStorage.mockRestore();
+  spyConsoleWarn.mockRestore();
 });
 
 it('Rollsback to a previous schema when triggered', async () => {
