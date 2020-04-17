efbddbfaddf139c5f00467a17673d743584e11b7
Jesse Rosenberger <git@jro.cc>
2020-04-17 08:10:41 +0300

    Update packages/apollo-server-core/src/ApolloServer.ts

--- packages/apollo-server-core/src/ApolloServer.ts
+++ packages/apollo-server-core/src/ApolloServer.ts
@@ -819,8 +819,12 @@ export class ApolloServerBase {
   protected async graphQLServerOptions(
     integrationContextArgument?: Record<string, any>,
   ): Promise<GraphQLServerOptions> {
-    const { schema, schemaHash, documentStore, extensions }
-      = await this.schemaDerivedData;
+    const {
+      schema,
+      schemaHash,
+      documentStore,
+      extensions,
+    } = await this.schemaDerivedData;
 
     let context: Context = this.context ? this.context : {};
 
