eea100aba16ba07c1a974d91bbbcf385c27f83d5
Blake Jackson <blakejackson@Blakes-MBP.lan>
2020-04-18 14:31:29 -0500

    Set up framework for new tests.

--- packages/apollo-gateway/src/__tests__/integration/requires.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/requires.test.ts
@@ -1,12 +1,16 @@
 import gql from 'graphql-tag';
-import { execute } from '../execution-utils';
+import { execute, ServiceDefinitionModule } from '../execution-utils';
+import { astSerializer, queryPlanSerializer } from '../../snapshotSerializers';
 import * as accounts from '../__fixtures__/schemas/accounts';
 import * as books from '../__fixtures__/schemas/books';
 import * as inventory from '../__fixtures__/schemas/inventory';
 import * as product from '../__fixtures__/schemas/product';
 import * as reviews from '../__fixtures__/schemas/reviews';
 
-it('supports passing additional fields defined by a requires', async () => {
+expect.addSnapshotSerializer(astSerializer);
+expect.addSnapshotSerializer(queryPlanSerializer);
+
+it('supports passing additional scalar fields defined by a requires', async () => {
   const query = gql`
     query GetReviwedBookNames {
       me {
@@ -46,4 +50,134 @@ it('supports passing additional fields defined by a requires', async () => {
   expect(queryPlan).toCallService('reviews');
   expect(queryPlan).toCallService('product');
   expect(queryPlan).toCallService('books');
+});
+
+
+const publisherService: ServiceDefinitionModule = {
+  name: 'publisher',
+  typeDefs: gql`
+    type Publisher {
+      id: ID!
+      name: String!
+    }
+
+    extend type User @key(fields: "id") {
+      id: ID! @external
+      organization: Organization! @external
+      publisher: Publisher! @requires(fields: "organization { name address { country } }")
+      publisherCity: String! @requires(fields: "organization { address { city coordinates { type value }}}")
+    }
+
+    type Organization {
+      name: String!
+      address: Address
+    }
+
+    type Address {
+      city: String!
+      country: String!
+      coordinates: [Coordinate!]!
+    }
+
+    type Coordinate {
+      type: CoordinateType!
+      value: Float!
+    }
+
+    enum CoordinateType {
+      LATITUDE
+      LONGITUDE
+    }
+  `,
+  resolvers: {
+    User: {
+      publisher(user) {
+        return {
+          id: 1,
+          name: user.organization.name + ' ' + user.organization.address.country,
+        }
+      },
+      publisherCity(user) {
+        return user.organization.address.city + ' ' + user.organization.address.coordinates.map((coordinate: any) => coordinate.value).join(' ');
+      },
+    },
+  },
+};
+
+const userService: ServiceDefinitionModule = {
+  name: 'user',
+  typeDefs: gql`
+    type Query {
+      me: User
+    }
+
+    type User @key(fields: "id") {
+      id: ID!
+      name: String!
+      organization: Organization!
+    }
+
+    type Organization {
+      name: String!
+      address: Address
+    }
+
+    type Address {
+      city: String!
+      country: String!
+      coordinates: [Coordinate!]!
+    }
+
+    type Coordinate {
+      type: CoordinateType!
+      value: Float!
+    }
+
+    enum CoordinateType {
+      LATITUDE
+      LONGITUDE
+    }
+  `,
+  resolvers: {
+    Query: {
+      me() {
+        return {
+          id: 'abc',
+          name: 'meme',
+          organization: {
+            name: 'org 1',
+            address: {
+              city: 'New York',
+              country: 'USA',
+              coordinates: [
+                {type: 'LATITUDE', value: 123},
+                {type: 'LONGITUDE', value: 321},
+              ]
+            }
+          }
+        }
+      }
+    }
+  },
+};
+
+it('supports passing additional deeply nested fields defined by a requires', async () => {
+  const query = gql`
+    query Me {
+      me {
+        name
+        publisher {
+          id
+          name
+        }
+        publisherCity
+      }
+    }
+  `;
+
+  const { data, queryPlan } = await execute([userService, publisherService], {
+    query,
+  });
+
+  console.log(data)
 });
