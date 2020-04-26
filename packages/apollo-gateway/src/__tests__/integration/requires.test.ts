bb4d9e62c8599c684d841b688907b42d4c157d77
Blake Jackson <blakejackson@Blakes-MBP.lan>
2020-04-26 12:53:53 -0500

    Updated tests to include requires on nested entities.

--- packages/apollo-gateway/src/__tests__/integration/requires.test.ts
+++ packages/apollo-gateway/src/__tests__/integration/requires.test.ts
@@ -67,7 +67,6 @@ const serviceA: ServiceDefinitionModule = {
       name: String!
       nested1: Nested1!
       nested2: Nested2!
-      nested3: Nested3!
     }
     type Nested1 {
       nameA: String!
@@ -81,23 +80,14 @@ const serviceA: ServiceDefinitionModule = {
       nameD: String!
       nested3: [Nested3!]!
     }
-    type Nested3 {
+    type Nested3 @key(fields: "id") {
+      id: ID!
       nameA: String!
       nameB: String!
       nameC: String!
-      nameD: String!
-      nameE: String!
       nested4: Nested4!
     }
-    type Nested4 @key(fields: "id") {
-      id: ID!
-      nameA: String!
-      nameB: String!
-      nameC: String!
-      nested5: Nested5!
-    }
-    type Nested5 {
-      id: ID!
+    type Nested4 {
       nameA: String!
       nameB: String!
     }
@@ -109,72 +99,42 @@ const serviceA: ServiceDefinitionModule = {
           id: '1',
           name: 'name',
           nested1: {
-            nameA: "nested1.nameA",
-            nameB: "nested1.nameB",
+            nameA: 'nested1.nameA',
+            nameB: 'nested1.nameB',
             nested2: {
-              nameA: "nested1.nested2.nameA",
-              nameB: "nested1.nested2.nameB",
-              nameC: "nested1.nested2.nameC",
-              nameD: "nested1.nested2.nameD",
+              nameA: 'nested1.nested2.nameA',
+              nameB: 'nested1.nested2.nameB',
+              nameC: 'nested1.nested2.nameC',
+              nameD: 'nested1.nested2.nameD',
               nested3: {
-                nameA: "nested1.nested2.nested3.nameA",
-                nameB: "nested1.nested2.nested3.nameB",
-                nameC: "nested1.nested2.nested3.nameC",
-                nameD: "nested1.nested2.nested3.nameD",
-                nameE: "nested1.nested2.nested3.nameE",
+                nameA: 'nested1.nested2.nested3.nameA',
+                nameB: 'nested1.nested2.nested3.nameB',
+                nameC: 'nested1.nested2.nested3.nameC',
                 nested4: {
-                  nameA: "nested1.nested2.nested3.nested4.nameA",
-                  nameB: "nested1.nested2.nested3.nested4.nameB",
-                  nameD: "nested1.nested2.nested3.nested4.nameC",
-                  nested5: {
-                    nameA: "nested1.nested2.nested3.nested4.nested5.nameA",
-                    nameB: "nested1.nested2.nested3.nested4.nested5.nameB",
-                  }
-                }
-              }
-            }
+                  nameA: 'nested1.nested2.nested3.nested4.nameA',
+                  nameB: 'nested1.nested2.nested3.nested4.nameB',
+                },
+              },
+            },
           },
           nested2: {
-            nameA: "nested2.nameA",
-            nameB: "nested2.nameB",
-            nameC: "nested2.nameC",
-            nameD: "nested2.nameD",
+            nameA: 'nested2.nameA',
+            nameB: 'nested2.nameB',
+            nameC: 'nested2.nameC',
+            nameD: 'nested2.nameD',
             nested3: {
-              nameA: "nested2.nested3.nameA",
-              nameB: "nested2.nested3.nameB",
-              nameC: "nested2.nested3.nameC",
-              nameD: "nested2.nested3.nameD",
-              nameE: "nested2.nested3.nameE",
+              nameA: 'nested2.nested3.nameA',
+              nameB: 'nested2.nested3.nameB',
+              nameC: 'nested2.nested3.nameC',
               nested4: {
-                nameA: "nested2.nested3.nested4.nameA",
-                nameB: "nested2.nested3.nested4.nameB",
-                nameC: "nested2.nested3.nested4.nameC",
-                nested5: {
-                  nameA: "nested2.nested3.nested4.nested5.nameA",
-                  nameB: "nested2.nested3.nested4.nested5.nameB",
-                }
-              }
-            }
+                nameA: 'nested2.nested3.nested4.nameA',
+                nameB: 'nested2.nested3.nested4.nameB',
+              },
+            },
           },
-          nested3: {
-            nameA: "nested3.nameA",
-            nameB: "nested3.nameB",
-            nameC: "nested3.nameC",
-            nameD: "nested3.nameD",
-            nameE: "nested3.nameE",
-            nested4: {
-              nameA: "nested3.nested4.nameA",
-              nameB: "nested3.nested4.nameB",
-              nameC: "nested3.nested4.nameC",
-              nested5: {
-                nameA: "nested3.nested4.nested5.nameA",
-                nameB: "nested3.nested4.nested5.nameB",
-              }
-            }
-          }
-        }
-      }
-    }
+        };
+      },
+    },
   },
 };
 
@@ -185,12 +145,20 @@ const serviceB: ServiceDefinitionModule = {
       id: ID! @external
       nested1: Nested1! @external
       nested2: Nested2! @external
-      nested3: Nested3! @external
-      calculated1: String! @requires(fields: "nested1 { nameA nested2 { nameA } }")
-      calculated2: String! @requires(fields: "nested1 { nameB  nested2 { nameB nested3 { nameA } } }")
-      calculated3: String! @requires(fields: "nested1 { nested2 { nested3 { nameB } } } nested2 { nameC nested3 { nameC } }")
-      calculated4: String! @requires(fields: "nested2 { nameD nested3 { nameD nested4 { nameA } } }")
-      calculated5: String! @requires(fields: "nested2 { nested3 { nested4 { nameB nested5 { nameA nameB } } } } nested3 { nameE nested4 { nameC } }")
+      calculated1: String!
+        @requires(fields: "nested1 { nameA nested2 { nameA } }")
+      calculated2: String!
+        @requires(
+          fields: "nested1 { nameB  nested2 { nameB nested3 { nameA } } }"
+        )
+      calculated3: String!
+        @requires(
+          fields: "nested1 { nested2 { nested3 { nameB } } } nested2 { nameC nested3 { nameC } }"
+        )
+      calculated4: String!
+        @requires(
+          fields: "nested2 { nameC nameD nested3 { nested4 { nameA } } }"
+        )
     }
     type Nested1 {
       nameA: String!
@@ -204,23 +172,15 @@ const serviceB: ServiceDefinitionModule = {
       nameD: String!
       nested3: [Nested3!]!
     }
-    type Nested3 {
-      nameA: String!
-      nameB: String!
-      nameC: String!
-      nameD: String!
-      nameE: String!
-      nested4: Nested4!
-    }
-    extend type Nested4 @key(fields: "id") {
+    extend type Nested3 @key(fields: "id") {
       id: ID! @external
       nameA: String! @external
       nameB: String! @external
       nameC: String! @external
-      nested5: Nested5! @external
+      nested4: Nested4! @external
+      calculated5: String! @requires(fields: "nested4 { nameB }")
     }
-    type Nested5 {
-      id: ID!
+    type Nested4 {
       nameA: String!
       nameB: String!
     }
@@ -231,26 +191,40 @@ const serviceB: ServiceDefinitionModule = {
         return parent.nested1.nameA + ' ' + parent.nested1.nested2.nameA;
       },
       calculated2(parent) {
-        return parent.nested1.nameB + ' ' + parent.nested1.nested2.nameB + ' ' + parent.nested1.nested2.nested3.nameA;
+        return (
+          parent.nested1.nameB +
+          ' ' +
+          parent.nested1.nested2.nameB +
+          ' ' +
+          parent.nested1.nested2.nested3.nameA
+        );
       },
       calculated3(parent) {
-        return parent.nested1.nested2.nested3.nameB + ' ' + parent.nested2.nameC + ' ' + parent.nested2.nested3.nameC;
+        return (
+          parent.nested1.nested2.nested3.nameB +
+          ' ' +
+          parent.nested2.nameC +
+          ' ' +
+          parent.nested2.nested3.nameC
+        );
       },
       calculated4(parent) {
-        return parent.nested2.nameD + ' ' + parent.nested2.nested3.nameD + ' ' + parent.nested2.nested3.nested4.nameA;
+        return (
+          parent.nested2.nameD +
+          ' ' +
+          parent.nested2.nested3.nameD +
+          ' ' +
+          parent.nested2.nested3.nested4.nameA
+        );
       },
+    },
+    Nested3: {
       calculated5(parent) {
-        return parent.nested2.nested3.nested4.nameB + ' '
-          + parent.nested2.nested3.nested4.nested5.nameA + ' '
-          + parent.nested2.nested3.nested4.nested5.nameB + ' '
-          + parent.nested3.nameA + ' '
-          + parent.nested3.nested4.nameC;
+        return parent.nested4.nameB;
       },
     },
   },
 };
-
-
 
 it('supports multiple arbitrarily nested fields defined by a requires', async () => {
   const query = gql`
@@ -261,7 +235,13 @@ it('supports multiple arbitrarily nested fields defined by a requires', async ()
         calculated2
         calculated3
         calculated4
-        calculated5
+        nested1 {
+          nested2 {
+            nested3 {
+              calculated5
+            }
+          }
+        }
       }
     }
   `;
