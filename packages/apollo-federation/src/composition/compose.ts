66f8845df7c0b7c8090736bd977d5bca9ddef4c3
Chase Cummings <Chase.Cummings@poly.com>
2020-05-21 23:10:33 -0700

    Insert check for existance of Subscription definition

--- packages/apollo-federation/src/composition/compose.ts
+++ packages/apollo-federation/src/composition/compose.ts
@@ -51,6 +51,12 @@ const EmptyMutationDefinition = {
   fields: [],
   serviceName: null,
 };
+const EmptySubscriptionDefinition = {
+  kind: Kind.OBJECT_TYPE_DEFINITION,
+  name: { kind: Kind.NAME, value: 'Subscription' },
+  fields: [],
+  serviceName: null,
+};
 
 // Map of all type definitions to eventually be passed to extendSchema
 interface TypeDefinitionsMap {
@@ -315,6 +321,8 @@ export function buildMapsFromServiceList(serviceList: ServiceDefinition[]) {
     typeDefinitionsMap.Query = [EmptyQueryDefinition];
   if (typeExtensionsMap.Mutation && !typeDefinitionsMap.Mutation)
     typeDefinitionsMap.Mutation = [EmptyMutationDefinition];
+  if (typeExtensionsMap.Subscription && !typeDefinitionsMap.Subscription)
+    typeDefinitionsMap.Subscription = [EmptySubscriptionDefinition];
 
   return {
     typeToServiceMap,
