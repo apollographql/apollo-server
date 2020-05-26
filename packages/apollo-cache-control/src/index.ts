342ed56b3491b60836c742800fee4ab51ad8b795
Steve Zimmerman <szman86@gmail.com>
2020-05-25 17:31:53 -0700

    cache-control set if 0

--- packages/apollo-cache-control/src/index.ts
+++ packages/apollo-cache-control/src/index.ts
@@ -201,7 +201,7 @@ export class CacheControlExtension<TContext = any>
 
     // If maxAge is 0, then we consider it uncacheable so it doesn't matter what
     // the scope was.
-    return lowestMaxAge
+    return lowestMaxAge >= 0
       ? {
           maxAge: lowestMaxAge,
           scope,
