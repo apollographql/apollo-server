a3ec950dd7afa915bb9435a73b78d4a67414873e
Mark Faga <mfaga@wayfair.com>
2020-05-27 14:23:11 -0400

    Allow typeDefs to be provided directly in serviceList

--- packages/apollo-gateway/src/loadServicesFromRemoteEndpoint.ts
+++ packages/apollo-gateway/src/loadServicesFromRemoteEndpoint.ts
@@ -1,8 +1,11 @@
 import { GraphQLRequest } from 'apollo-server-types';
-import { parse } from 'graphql';
+import { parse, DocumentNode } from 'graphql';
 import { Headers, HeadersInit } from 'node-fetch';
 import { GraphQLDataSource } from './datasources/types';
-import { Experimental_UpdateServiceDefinitions, SERVICE_DEFINITION_QUERY } from './';
+import {
+  Experimental_UpdateServiceDefinitions,
+  SERVICE_DEFINITION_QUERY,
+} from './';
 import { ServiceDefinition } from '@apollo/federation';
 
 export async function getServiceDefinitionsFromRemoteEndpoint({
@@ -13,6 +16,7 @@ export async function getServiceDefinitionsFromRemoteEndpoint({
   serviceList: {
     name: string;
     url?: string;
+    localTypeDefs?: DocumentNode;
     dataSource: GraphQLDataSource;
   }[];
   headers?: HeadersInit;
@@ -26,51 +30,64 @@ export async function getServiceDefinitionsFromRemoteEndpoint({
 
   let isNewSchema = false;
   // for each service, fetch its introspection schema
-  const promiseOfServiceList = serviceList.map(({ name, url, dataSource }) => {
-    if (!url) {
-      throw new Error(
-        `Tried to load schema for '${name}' but no 'url' was specified.`);
-    }
+  const promiseOfServiceList = serviceList.map(
+    ({ name, url, localTypeDefs, dataSource }) => {
+      if (!url) {
+        throw new Error(
+          `Tried to load schema for '${name}' but no 'url' was specified.`,
+        );
+      }
 
-    const request: GraphQLRequest = {
-      query: SERVICE_DEFINITION_QUERY,
-      http: {
-        url,
-        method: 'POST',
-        headers: new Headers(headers),
-      },
-    };
+      const request: GraphQLRequest = {
+        query: SERVICE_DEFINITION_QUERY,
+        http: {
+          url,
+          method: 'POST',
+          headers: new Headers(headers),
+        },
+      };
 
-    return dataSource
-      .process({ request, context: {} })
-      .then(({ data, errors }): ServiceDefinition => {
-        if (data && !errors) {
-          const typeDefs = data._service.sdl as string;
-          const previousDefinition = serviceSdlCache.get(name);
-          // this lets us know if any downstream service has changed
-          // and we need to recalculate the schema
-          if (previousDefinition !== typeDefs) {
-            isNewSchema = true;
-          }
-          serviceSdlCache.set(name, typeDefs);
-          return {
-            name,
-            url,
-            typeDefs: parse(typeDefs),
-          };
-        }
+      if (localTypeDefs) {
+        return Promise.resolve({
+          name,
+          url,
+          typeDefs: localTypeDefs,
+        });
+      }
 
-        throw new Error(errors?.map(e => e.message).join("\n"));
-      })
-      .catch(err => {
-        const errorMessage =
-          `Couldn't load service definitions for "${name}" at ${url}` +
-          (err && err.message ? ": " + err.message || err : "");
+      return dataSource
+        .process({ request, context: {} })
+        .then(
+          ({ data, errors }): ServiceDefinition => {
+            if (data && !errors) {
+              const typeDefs = data._service.sdl as string;
+              const previousDefinition = serviceSdlCache.get(name);
+              // this lets us know if any downstream service has changed
+              // and we need to recalculate the schema
+              if (previousDefinition !== typeDefs) {
+                isNewSchema = true;
+              }
+              serviceSdlCache.set(name, typeDefs);
+              return {
+                name,
+                url,
+                typeDefs: parse(typeDefs),
+              };
+            }
 
-        throw new Error(errorMessage);
-      });
-  });
+            throw new Error(errors?.map((e) => e.message).join('\n'));
+          },
+        )
+        .catch((err) => {
+          const errorMessage =
+            `Couldn't load service definitions for "${name}" at ${url}` +
+            (err && err.message ? ': ' + err.message || err : '');
+
+          throw new Error(errorMessage);
+        });
+    },
+  );
 
   const serviceDefinitions = await Promise.all(promiseOfServiceList);
-  return { serviceDefinitions, isNewSchema }
+  return { serviceDefinitions, isNewSchema };
 }
