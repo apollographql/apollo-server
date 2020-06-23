15ac1ccee810a91a822184bbd5af78cbe3e3a0bc
Mikhail Pospelov <m.pospelov@me.com>
2020-06-23 14:16:13 +0300

    feat: add edge case for value interface definition with entry service having no interface

---
+++ packages/apollo-gateway/src/__tests__/integration/unions-with-value-interfaces-distributed.test.ts
@@ -0,0 +1,225 @@
+import gql from 'graphql-tag';
+import { astSerializer, queryPlanSerializer } from '../../snapshotSerializers';
+import { execute, ServiceDefinitionModule } from '../execution-utils';
+
+expect.addSnapshotSerializer(astSerializer);
+expect.addSnapshotSerializer(queryPlanSerializer);
+
+const videos = [{ id: 1, url: 'https://foobar.com/videos/1' }];
+const articles = [{ id: 1, url: 'https://foobar.com/articles/1' }];
+const audios = [{ id: 1, audioUrl: 'https://foobar.com/audios/1' }];
+
+const contentService: ServiceDefinitionModule = {
+  name: 'contentService',
+  typeDefs: gql`
+    type Query {
+      content: [Content!]!
+    }
+    union Content = Video | Article | Audio
+    extend type Video @key(fields: "id") {
+      id: ID @external
+    }
+    extend type Audio @key(fields: "id") {
+      id: ID @external
+    }
+    extend type Article @key(fields: "id") {
+      id: ID @external
+    }
+  `,
+  resolvers: {
+    Query: {
+      content() {
+        return [
+          ...articles.map((a) => ({ ...a, type: 'Article' })),
+          ...audios.map(({ id }) => ({ id, type: 'Audio' })),
+          ...videos.map(({ id }) => ({ id, type: 'Video' })),
+        ];
+      },
+    },
+    Content: {
+      __resolveType(object) {
+        return object.type;
+      },
+    },
+  },
+};
+
+const articleService: ServiceDefinitionModule = {
+  name: 'articleService',
+  typeDefs: gql`
+    interface WebResource {
+      url: String
+    }
+    type Article implements WebResource @key(fields: "id") {
+      id: ID
+      url: String
+    }
+  `,
+  resolvers: {
+    Article: {
+      __resolveReference(object) {
+        return articles.find(
+          (article) => article.id === parseInt(object.id, 10),
+        );
+      },
+      id(object) {
+        return object.id;
+      },
+      url(object) {
+        return object.url;
+      },
+    },
+  },
+};
+
+const videoService: ServiceDefinitionModule = {
+  name: 'videoService',
+  typeDefs: gql`
+    interface WebResource {
+      url: String
+    }
+    type Video implements WebResource @key(fields: "id") {
+      id: ID
+      url: String
+    }
+  `,
+  resolvers: {
+    Video: {
+      __resolveReference(object) {
+        return videos.find((video) => video.id === parseInt(object.id, 10));
+      },
+      id(object) {
+        return object.id;
+      },
+      url(object) {
+        return object.url;
+      },
+    },
+  },
+};
+
+const audioService: ServiceDefinitionModule = {
+  name: 'audioService',
+  typeDefs: gql`
+    type Audio @key(fields: "id") {
+      id: ID
+      audioUrl: String
+    }
+  `,
+  resolvers: {
+    Audio: {
+      __resolveReference(object) {
+        return audios.find((audio) => audio.id === parseInt(object.id, 10));
+      },
+      id(object) {
+        return object.id;
+      },
+      audioUrl(object) {
+        return object.audioUrl;
+      },
+    },
+  },
+};
+
+it('handles unions from different services which implements value interfaces', async () => {
+  const query = `#graphql
+    query {
+      content {
+        ... on WebResource {
+          url
+        }
+        ... on Audio {
+          url: audioUrl
+        }
+      }
+    }
+  `;
+
+  const { queryPlan, errors, data } = await execute({ query }, [
+    contentService,
+    videoService,
+    audioService,
+    articleService,
+  ]);
+  expect(errors).toBeUndefined();
+
+  expect(queryPlan).toMatchInlineSnapshot(`
+    QueryPlan {
+      Sequence {
+        Fetch(service: "contentService") {
+          {
+            content {
+              __typename
+              ... on Video {
+                __typename
+                id
+              }
+              ... on Article {
+                __typename
+                id
+              }
+              ... on Audio {
+                __typename
+                id
+              }
+            }
+          }
+        },
+        Parallel {
+          Flatten(path: "content.@") {
+            Fetch(service: "videoService") {
+              {
+                ... on Video {
+                  __typename
+                  id
+                }
+              } =>
+              {
+                ... on Video {
+                  url
+                }
+              }
+            },
+          },
+          Flatten(path: "content.@") {
+            Fetch(service: "articleService") {
+              {
+                ... on Article {
+                  __typename
+                  id
+                }
+              } =>
+              {
+                ... on Article {
+                  url
+                }
+              }
+            },
+          },
+          Flatten(path: "content.@") {
+            Fetch(service: "audioService") {
+              {
+                ... on Audio {
+                  __typename
+                  id
+                }
+              } =>
+              {
+                ... on Audio {
+                  url: audioUrl
+                }
+              }
+            },
+          },
+        },
+      },
+    }
+  `);
+  expect(data).toEqual({
+    content: [
+      { url: 'https://foobar.com/articles/1' },
+      { url: 'https://foobar.com/audios/1' },
+      { url: 'https://foobar.com/videos/1' },
+    ],
+  });
+});
