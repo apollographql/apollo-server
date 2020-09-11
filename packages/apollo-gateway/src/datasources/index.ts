811b7576243c2fb82e11016dafce9c5294e18ec9
Jesse Rosenberger <git@jro.cc>
2020-09-11 14:25:43 +0000

    Move Federation (including Gateway) to another repository.
    Everything will continue, however, as federation becomes more independent
    from Apollo Server, it (and its concerns, like Apollo Gateway) move to their
    own repository.  (It's always been relatively independent, but it didn't
    necessarily warrant its own repository before.)
    
    This includes `apollo-federation-integration-testsuite` (unpublished to
    npm), `@apollo/gateway (which lived in `packages/apollo-gateway` here and as
    `/gateway-js` on the new repository), and `@apollo/federation` (which lived
    in `packages/apollo-federation` here and as `/federation-js` on the new
    repository).
    
    See https://github.com/apollographql/apollo-server/issues/4560 for details,
    and also https://github.com/apollographql/federation/pull/134 for where (and
    how) it was introduced on the other side.

--- packages/apollo-gateway/src/datasources/index.ts
+++
@@ -1,3 +0,0 @@
-export { LocalGraphQLDataSource } from './LocalGraphQLDataSource';
-export { RemoteGraphQLDataSource } from './RemoteGraphQLDataSource';
-export { GraphQLDataSource } from './types';
