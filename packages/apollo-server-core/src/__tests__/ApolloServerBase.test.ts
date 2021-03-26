apollographql/apollo-server

commit a3282a2d7df0c20d9e10b058defae835120fa5b1

Add async server.start() function (#4981)
Previously, server startup worked like this:

- `new ApolloServer`
  - If no gateway, calculate schema and schema derived data immediately
  - If gateway, kick off gateway.load from the end of the constructor, and if it
    async-throws, log an error once and make the server kinda broken forever
- At various spots in the framework integration code, call (but don't await)
  the protected `willStart` function, which is an async function that first
  waits for the gateway to load the schema if necessary and then runs
  serverWillStart plugin functions; save the Promise returned by calling this.
- At request time in the framework integration code, await that Promise.
  And also, if there's no schema, fail with an error.

Now server startup works like this:
- ApolloServer represents its state explicitly with a new ServerState
- `new ApolloServer`
  - If no gateway, initialize all the schema-derived state directly like
    before (though the state now lives inside ServerState)
  - If gateway, the constructor DOES NOT KICK OFF `gateway.load()`
- You can now call `await server.start()` yourself, which will first await
  `gateway.load` if necessary, and then await all serverWillStart calls.
- If you're using `apollo-server` rather than an integration, `server.listen()`
  will just transparently do this for you; explicit `start()` is just for
  integrations!
- Serverless frameworks also call it automatically for you in the background
  (kicked off by the constructor) because their startup has to be
  synchronous; if it fails then future requests will all fail (and log) as before.
- The integration places that used to call willStart now call
  `server.ensureStarting()` instead which will kick off server.start in the
  background if you didn't (and log any errors thrown).
- The places that used to await promiseWillStart no longer do so; generally

packages/apollo-server-core/src/__tests__/ApolloServerBase.test.ts
@@ -37,16 +37,15 @@ describe('ApolloServerBase construction', () => {
 
   it('succeeds when passed a graphVariant in construction', () => {
     let serverBase;
-    expect(
-      () =>
-        new ApolloServerBase({
-          typeDefs,
-          resolvers,
-          engine: {
-            graphVariant: 'foo',
-            apiKey: 'not:real:key',
-          },
-        }).stop()
+    expect(() =>
+      new ApolloServerBase({
+        typeDefs,
+        resolvers,
+        engine: {
+          graphVariant: 'foo',
+          apiKey: 'not:real:key',
+        },
+      }).stop(),
     ).not.toThrow();
   });
 
@@ -88,7 +87,7 @@ describe('ApolloServerBase construction', () => {
         schema: {},
       });
     }).toThrowErrorMatchingInlineSnapshot(
-      `"Unexpected error: Unable to resolve a valid GraphQLSchema.  Please file an issue with a reproduction of this error, if possible."`,
+      `"Expected {} to be a GraphQL schema."`,
     );
   });
 

packages/apollo-server-azure-functions/src/ApolloServer.ts
@@ -36,11 +36,6 @@ export class ApolloServer extends ApolloServerBase {
   }
 
   public createHandler({ cors }: CreateHandlerOptions = { cors: undefined }) {
-    // We will kick off the `willStart` event once for the server, and then
-    // await it before processing any requests by incorporating its `await` into
-    // the GraphQLServerOptions function which is called before each request.
-    const promiseWillStart = this.willStart();
-
     const corsHeaders: HttpResponse['headers'] = {};
 
     if (cors) {
@@ -144,7 +139,6 @@ export class ApolloServer extends ApolloServerBase {
         );
       };
       graphqlAzureFunction(async () => {
-        await promiseWillStart;
         return this.createGraphQLServerOptions(req, context);
       })(context, req, callbackFilter);
     };

packages/apollo-server-cloud-functions/src/ApolloServer.ts
@@ -34,11 +34,6 @@ export class ApolloServer extends ApolloServerBase {
   }
 
   public createHandler({ cors }: CreateHandlerOptions = { cors: undefined }) {
-    // We will kick off the `willStart` event once for the server, and then
-    // await it before processing any requests by incorporating its `await` into
-    // the GraphQLServerOptions function which is called before each request.
-    const promiseWillStart = this.willStart();
-
     const corsHeaders = {} as Record<string, any>;
 
     if (cors) {
@@ -129,14 +124,6 @@ export class ApolloServer extends ApolloServerBase {
       }
 
       graphqlCloudFunction(async () => {
-        // In a world where this `createHandler` was async, we might avoid this
-        // but since we don't want to introduce a breaking change to this API
-        // (by switching it to `async`), we'll leverage the
-        // `GraphQLServerOptions`, which are dynamically built on each request,
-        // to `await` the `promiseWillStart` which we kicked off at the top of
-        // this method to ensure that it runs to completion (which is part of
-        // its contract) prior to processing the request.
-        await promiseWillStart;
         return this.createGraphQLServerOptions(req, res);
       })(req, res);
     };

packages/apollo-server-cloudflare/src/ApolloServer.ts
@@ -14,7 +14,11 @@ export class ApolloServer extends ApolloServerBase {
   }
 
   public async listen() {
-    await this.willStart();
+    // In case the user didn't bother to call and await the `start` method, we
+    // kick it off in the background (with any errors getting logged
+    // and also rethrown from graphQLServerOptions during later requests).
+    this.ensureStarting();
+
     addEventListener('fetch', (event: FetchEvent) => {
       event.respondWith(
         graphqlCloudflare(() => {

packages/apollo-server-express/src/ApolloServer.ts
@@ -122,25 +122,12 @@ export class ApolloServer extends ApolloServerBase {
   }: GetMiddlewareOptions = {}): express.Router {
     if (!path) path = '/graphql';
 
+    // In case the user didn't bother to call and await the `start` method, we
+    // kick it off in the background (with any errors getting logged
+    // and also rethrown from graphQLServerOptions during later requests).
+    this.ensureStarting();
+
     const router = express.Router();
-
-    // Despite the fact that this `applyMiddleware` function is `async` in
-    // other integrations (e.g. Hapi), currently it is not for Express (@here).
-    // That should change in a future version, but that would be a breaking
-    // change right now (see comment above this method's declaration above).
-    //
-    // That said, we do need to await the `willStart` lifecycle event which
-    // can perform work prior to serving a request.  Since Express doesn't
-    // natively support Promises yet, we'll do this via a middleware that
-    // calls `next` when the `willStart` finishes.  We'll kick off the
-    // `willStart` right away, so hopefully it'll finish before the first
-    // request comes in, but we won't call `next` on this middleware until it
-    // does. (And we'll take care to surface any errors via the `.catch`-able.)
-    const promiseWillStart = this.willStart();
-
-    router.use(path, (_req, _res, next) => {
-      promiseWillStart.then(() => next()).catch(next);
-    });
 
     if (!disableHealthCheck) {
       router.use('/.well-known/apollo/server-health', (req, res) => {

packages/apollo-server-fastify/src/ApolloServer.ts
@@ -85,13 +85,15 @@ export class ApolloServer extends ApolloServerBase {
     onHealthCheck,
   }: ServerRegistration = {}) {
     this.graphqlPath = path ? path : '/graphql';
-    const promiseWillStart = this.willStart();
+
+    // In case the user didn't bother to call and await the `start` method, we
+    // kick it off in the background (with any errors getting logged
+    // and also rethrown from graphQLServerOptions during later requests).
+    this.ensureStarting();
 
     return async (
       app: FastifyInstance<Server, IncomingMessage, ServerResponse>,
     ) => {
-      await promiseWillStart;
-
       if (!disableHealthCheck) {
         app.get('/.well-known/apollo/server-health', async (req, res) => {
           // Response follows https://tools.ietf.org/html/draft-inadarei-api-health-check-01

packages/apollo-server-hapi/src/ApolloServer.ts
@@ -60,7 +60,10 @@ export class ApolloServer extends ApolloServerBase {
     disableHealthCheck,
     onHealthCheck,
   }: ServerRegistration) {
-    await this.willStart();
+    // In case the user didn't bother to call and await the `start` method, we
+    // kick it off in the background (with any errors getting logged
+    // and also rethrown from graphQLServerOptions during later requests).
+    this.ensureStarting();
 
     if (!path) path = '/graphql';
 

commit 285c5a527e19dd09135002a62831af32a9c0d235

Ensure startup errors are redacted even the first time
This is a regression in #4981. If the server start process is begun implicitly
by the execution of an operation (ensureStarted inside graphQLServerOptions) and
startup throws, the log-and-redact logic wasn't being invoked.

Note that this case doesn't usually happen in practice, because:
- If you're using `apollo-server`, startup is begun in `listen()` before you can
  serve requests
- If you're using a serverless framework integration, startup is begun in the
  constructor
- If you're using a non-serverless framework integration, the function you call
  to connect it to your framework begins startup with `ensureStarting()`

So mostly this just affects the case that you're running `executeOperation`
without calling `start()` or `listen()`, or maybe you have your own custom
framework integration that doesn't call `ensureStarting()`. But it's still worth
missing.

Add some tests of this behavior and fix some TypeScript issues in the test file.

packages/apollo-server-core/src/ApolloServer.ts
@@ -623,7 +623,12 @@ export class ApolloServerBase {
       switch (this.state.phase) {
         case 'initialized with gateway':
         case 'initialized with schema':
-          await this._start();
+          try {
+            await this._start();
+          } catch {
+            // Any thrown error should transition us to 'failed to start', and
+            // we'll handle that on the next iteration of the while loop.
+          }
           // continue the while loop
           break;
         case 'starting':
@@ -632,9 +637,8 @@ export class ApolloServerBase {
           // continue the while loop
           break;
         case 'failed to start':
-          //  First
-          // we log the error that prevented startup (which means it will get logged
-          // once for every GraphQL operation).
+          // First we log the error that prevented startup (which means it will
+          // get logged once for every GraphQL operation).
           this.logStartupError(this.state.error);
           // Now make the operation itself fail.
           // We intentionally do not re-throw actual startup error as it may contain

packages/apollo-server-core/src/__tests__/ApolloServerBase.test.ts
@@ -2,6 +2,8 @@ import { ApolloServerBase } from '../ApolloServer';
 import { buildServiceDefinition } from '@apollographql/apollo-tools';
 import gql from 'graphql-tag';
 import { Logger } from 'apollo-server-types';
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+import type { GraphQLSchema } from 'graphql';
 
 const typeDefs = gql`
   type Query {
@@ -36,7 +38,6 @@ describe('ApolloServerBase construction', () => {
   });
 
   it('succeeds when passed a graphVariant in construction', () => {
-    let serverBase;
     expect(() =>
       new ApolloServerBase({
         typeDefs,
@@ -84,7 +85,7 @@ describe('ApolloServerBase construction', () => {
   it('throws when a GraphQLSchema is not provided to the schema configuration option', () => {
     expect(() => {
       new ApolloServerBase({
-        schema: {},
+        schema: {} as GraphQLSchema,
       });
     }).toThrowErrorMatchingInlineSnapshot(
       `"Expected {} to be a GraphQL schema."`,
@@ -96,6 +97,58 @@ describe('ApolloServerBase construction', () => {
       new ApolloServerBase({});
     }).toThrowErrorMatchingInlineSnapshot(
       `"Apollo Server requires either an existing schema, modules or typeDefs"`,
+    );
+  });
+});
+
+describe('ApolloServerBase start', () => {
+  const failToStartPlugin: ApolloServerPlugin = {
+    async serverWillStart() {
+      throw Error('nope');
+    },
+  };
+  const redactedMessage =
+    'This data graph is missing a valid configuration. More details may be available in the server logs.';
+
+  it('start throws on startup error', async () => {
+    const server = new ApolloServerBase({
+      typeDefs,
+      resolvers,
+      plugins: [failToStartPlugin],
+    });
+    await expect(server.start()).rejects.toThrow('nope');
+  });
+
+  it('execute throws redacted message on implicit startup error', async () => {
+    const error = jest.fn();
+    const logger: Logger = {
+      debug: jest.fn(),
+      info: jest.fn(),
+      warn: jest.fn(),
+      error,
+    };
+
+    const server = new ApolloServerBase({
+      typeDefs,
+      resolvers,
+      plugins: [failToStartPlugin],
+      logger,
+    });
+    // Run the operation twice (the first will kick off the start process). We
+    // want to see the same error thrown and log message for the "kick it off"
+    // call as the subsequent call.
+    await expect(
+      server.executeOperation({ query: '{__typename}' }),
+    ).rejects.toThrow(redactedMessage);
+    await expect(
+      server.executeOperation({ query: '{__typename}' }),
+    ).rejects.toThrow(redactedMessage);
+    expect(error).toHaveBeenCalledTimes(2);
+    expect(error.mock.calls[0][0]).toMatch(
+      /Apollo Server was started implicitly.*nope/,
+    );
+    expect(error.mock.calls[1][0]).toMatch(
+      /Apollo Server was started implicitly.*nope/,
     );
   });
 });