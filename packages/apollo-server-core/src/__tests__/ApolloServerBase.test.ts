97dbe7d15d8e5274321eb9262c22deeaf95bb7fe
David Glasser <glasser@apollographql.com>
2021-03-25 21:55:32 -0700

    Ensure startup errors are redacted even the first time (#5064)
    This is a regression in #4981. If the server start process is begun implicitly
    by the execution of an operation (ensureStarted inside graphQLServerOptions) and
    startup throws, the log-and-redact logic wasn't being invoked.
    
    Note that this case doesn't usually happen in practice, because:
    - If you're using `apollo-server`, startup is begun in `listen()` before you can
      serve requests
    - If you're using a serverless framework integration, startup is begun in the
      constructor
    - If you're using a non-serverless framework integration, the function you call
      to connect it to your framework begins startup with `ensureStarting()`
    
    So mostly this just affects the case that you're running `executeOperation`
    without calling `start()` or `listen()`, or maybe you have your own custom
    framework integration that doesn't call `ensureStarting()`. But it's still worth
    missing.
    
    Add some tests of this behavior and fix some TypeScript issues in the test file.

--- packages/apollo-server-core/src/__tests__/ApolloServerBase.test.ts
+++ packages/apollo-server-core/src/__tests__/ApolloServerBase.test.ts
@@ -2,6 +2,8 @@ import { ApolloServerBase } from '../ApolloServer';
 import { buildServiceDefinition } from '@apollographql/apollo-tools';
 import gql from 'graphql-tag';
 import { Logger } from 'apollo-server-types';
+import { ApolloServerPlugin } from 'apollo-server-plugin-base';
+import type { GraphQLSchema } from 'graphql';
 
 const typeDefs = gql`
   type Query {
@@ -36,7 +38,6 @@ describe('ApolloServerBase construction', () => {
   });
 
   it('succeeds when passed a graphVariant in construction', () => {
-    let serverBase;
     expect(() =>
       new ApolloServerBase({
         typeDefs,
@@ -84,7 +85,7 @@ describe('ApolloServerBase construction', () => {
   it('throws when a GraphQLSchema is not provided to the schema configuration option', () => {
     expect(() => {
       new ApolloServerBase({
-        schema: {},
+        schema: {} as GraphQLSchema,
       });
     }).toThrowErrorMatchingInlineSnapshot(
       `"Expected {} to be a GraphQL schema."`,
@@ -96,6 +97,58 @@ describe('ApolloServerBase construction', () => {
       new ApolloServerBase({});
     }).toThrowErrorMatchingInlineSnapshot(
       `"Apollo Server requires either an existing schema, modules or typeDefs"`,
+    );
+  });
+});
+
+describe('ApolloServerBase start', () => {
+  const failToStartPlugin: ApolloServerPlugin = {
+    async serverWillStart() {
+      throw Error('nope');
+    },
+  };
+  const redactedMessage =
+    'This data graph is missing a valid configuration. More details may be available in the server logs.';
+
+  it('start throws on startup error', async () => {
+    const server = new ApolloServerBase({
+      typeDefs,
+      resolvers,
+      plugins: [failToStartPlugin],
+    });
+    await expect(server.start()).rejects.toThrow('nope');
+  });
+
+  it('execute throws redacted message on implicit startup error', async () => {
+    const error = jest.fn();
+    const logger: Logger = {
+      debug: jest.fn(),
+      info: jest.fn(),
+      warn: jest.fn(),
+      error,
+    };
+
+    const server = new ApolloServerBase({
+      typeDefs,
+      resolvers,
+      plugins: [failToStartPlugin],
+      logger,
+    });
+    // Run the operation twice (the first will kick off the start process). We
+    // want to see the same error thrown and log message for the "kick it off"
+    // call as the subsequent call.
+    await expect(
+      server.executeOperation({ query: '{__typename}' }),
+    ).rejects.toThrow(redactedMessage);
+    await expect(
+      server.executeOperation({ query: '{__typename}' }),
+    ).rejects.toThrow(redactedMessage);
+    expect(error).toHaveBeenCalledTimes(2);
+    expect(error.mock.calls[0][0]).toMatch(
+      /Apollo Server was started implicitly.*nope/,
+    );
+    expect(error.mock.calls[1][0]).toMatch(
+      /Apollo Server was started implicitly.*nope/,
     );
   });
 });
