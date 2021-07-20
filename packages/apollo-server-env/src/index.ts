1305c83b323e0bfdf668c135d2b1f5830a49d919
David Glasser <glasser@davidglasser.net>
2021-07-19 21:07:19 -0700

    apollo-server-env: remove Node 6 compatibility polyfills
    Node 6 support meant we needed to polyfill util.promisify,
    Object.values, and Object.entries. But we now only support Node 12+.
    
    Fixes #5478.

--- packages/apollo-server-env/src/index.ts
+++ packages/apollo-server-env/src/index.ts
@@ -1,11 +1,2 @@
-import './polyfills/Object.values';
-import './polyfills/Object.entries';
-
-import runtimeSupportsPromisify from './utils/runtimeSupportsPromisify';
-
-if (!runtimeSupportsPromisify) {
-  require('util.promisify').shim();
-}
-
 export * from './polyfills/fetch';
 export * from './polyfills/url';
