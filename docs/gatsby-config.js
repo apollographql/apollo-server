fd892a0188febefce4b2f804e264445eab63e7c8
Trevor Blades <tdblades@gmail.com>
2021-07-22 23:33:20 -0700

    Upgrade gatsby theme and configure algolia indexing

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,13 +1,14 @@
 const themeOptions = require('gatsby-theme-apollo-docs/theme-options');
 
 module.exports = {
-  pathPrefix: '/docs/apollo-server',
   plugins: [
     {
       resolve: 'gatsby-theme-apollo-docs',
       options: {
         ...themeOptions,
         root: __dirname,
+        pathPrefix: '/docs/apollo-server',
+        algoliaIndexName: 'server',
         subtitle: 'Apollo Server',
         description: 'A guide to using Apollo Server',
         githubRepo: 'apollographql/apollo-server',
