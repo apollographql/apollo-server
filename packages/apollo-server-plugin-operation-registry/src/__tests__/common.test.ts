f40d6981c049edb4541f97f23ff36e555b9b24e0
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2022-02-17 13:44:54 -0500

    Add tests

--- packages/apollo-server-plugin-operation-registry/src/__tests__/common.test.ts
+++ packages/apollo-server-plugin-operation-registry/src/__tests__/common.test.ts
@@ -4,4 +4,72 @@ describe('common', () => {
   it('uses the correct cache prefix', () => {
     expect(common.getStoreKey('abc123')).toStrictEqual('abc123');
   });
+
+  describe('urlOperationManifestBase', () => {
+    const OLD_ENV = process.env;
+    beforeEach(() => {
+      jest.resetModules()
+      process.env = { ...OLD_ENV };
+    });
+    afterAll(() => {
+      process.env = OLD_ENV;
+    });
+
+    it('evals to the override url when overridden', () => {
+      process.env.APOLLO_OPERATION_MANIFEST_BASE_URL = 'override';
+      const { urlOperationManifestBase } = require('../common');
+      expect(urlOperationManifestBase).toStrictEqual('override');
+    });
+
+    it('removes trailing slashes from the override url when overridden', () => {
+      process.env.APOLLO_OPERATION_MANIFEST_BASE_URL = 'override/';
+      const { urlOperationManifestBase } = require('../common');
+      expect(urlOperationManifestBase).toStrictEqual('override');
+    });
+
+    it('evals to the test url when testing', () => {
+      process.env.__APOLLO_OPERATION_REGISTRY_TESTS__ = 'true';
+      const { urlOperationManifestBase, fakeTestBaseUrl } = require('../common');
+      expect(urlOperationManifestBase).toStrictEqual(fakeTestBaseUrl);
+    });
+
+    it('evals to the default value when not overridden or testing', () => {
+      const { urlOperationManifestBase } = require('../common');
+      expect(urlOperationManifestBase).toStrictEqual('https://operations.api.apollographql.com');
+    });
+  });
+
+  describe('urlStorageSecretBase', () => {
+    const OLD_ENV = process.env;
+    beforeEach(() => {
+      jest.resetModules()
+      process.env = { ...OLD_ENV };
+    });
+    afterAll(() => {
+      process.env = OLD_ENV;
+    });
+    
+    it('evals to the override url when overridden', () => {
+      process.env.APOLLO_STORAGE_SECRET_BASE_URL = 'override';
+      const { urlStorageSecretBase } = require('../common');
+      expect(urlStorageSecretBase).toStrictEqual('override');
+    });
+
+    it('removes trailing slashes from the override url when overridden', () => {
+      process.env.APOLLO_STORAGE_SECRET_BASE_URL = 'override/';
+      const { urlStorageSecretBase } = require('../common');
+      expect(urlStorageSecretBase).toStrictEqual('override');
+    });
+
+    it('evals to the test url when testing', () => {
+      process.env.__APOLLO_OPERATION_REGISTRY_TESTS__ = 'true';
+      const { urlStorageSecretBase, fakeTestBaseUrl } = require('../common');
+      expect(urlStorageSecretBase).toStrictEqual(fakeTestBaseUrl);
+    });
+
+    it('evals to the default value when not overridden or testing', () => {
+      const { urlStorageSecretBase } = require('../common');
+      expect(urlStorageSecretBase).toStrictEqual('https://storage-secrets.api.apollographql.com');
+    });
+  });
 });
