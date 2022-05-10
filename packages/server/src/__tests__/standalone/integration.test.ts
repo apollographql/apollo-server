b98a345d00afa2d99f60271265be02df8cfd8f5c
Trevor Scheer <trevor.scheer@gmail.com>
2022-05-10 16:00:55 -0700

    prettier

--- packages/server/src/__tests__/standalone/integration.test.ts
+++ packages/server/src/__tests__/standalone/integration.test.ts
@@ -1,14 +1,12 @@
 import { httpServer as getHttpServer } from '../../standalone';
 import type { BaseContext } from '../../externalTypes';
-import type {
-  CreateServerForIntegrationTestsResult,
-} from '../integration';
+import type { CreateServerForIntegrationTestsResult } from '../integration';
 import { defineIntegrationTestSuite } from '../integration';
 import type { ApolloServerOptions } from '../../types';
 import { ApolloServer } from '../../ApolloServer';
 
 defineIntegrationTestSuite(async function (
-  serverOptions: ApolloServerOptions<BaseContext>
+  serverOptions: ApolloServerOptions<BaseContext>,
 ): Promise<CreateServerForIntegrationTestsResult> {
   const server = new ApolloServer(serverOptions);
   const httpServer = getHttpServer(server);
