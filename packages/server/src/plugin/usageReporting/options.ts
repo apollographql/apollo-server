01540846cf66ded047b135d61559868d01cce208
David Glasser <glasser@davidglasser.net>
2022-08-04 12:09:35 -0700

    Port #6763 from AS3
    Usage reporting: fix TS declaration of fieldLevelInstrumentation
    
    Note that the `const fieldLevelInstrumentation` which this got assigned
    to actually already inferred (roughly) the correct type because the type
    got merged with a number-returning function from another case.

--- packages/server/src/plugin/usageReporting/options.ts
+++ packages/server/src/plugin/usageReporting/options.ts
@@ -123,7 +123,7 @@ export interface ApolloServerPluginUsageReportingOptions<
     | number
     | ((
         request: GraphQLRequestContextDidResolveOperation<TContext>,
-      ) => Promise<boolean>);
+      ) => Promise<number | boolean>);
 
   /**
    * This option allows you to choose if a particular request should be
