363fd308d09c8df0428b059cbe6743a723ac65b2
Nick Marsh <bonnici@users.noreply.github.com>
2022-08-12 09:11:40 +1000

    Added unit tests for masked and unmodified error logging options (#6795)
    Co-authored-by: David Glasser <glasser@davidglasser.net>

--- packages/integration-testsuite/src/apolloServerTests.ts
+++ packages/integration-testsuite/src/apolloServerTests.ts
@@ -1310,6 +1310,108 @@ export function defineIntegrationTestSuiteApolloServerTests(
                 },
               ]);
             });
+
+            it('unmodified', async () => {
+              throwError.mockImplementationOnce(() => {
+                throw new GraphQLError('should be unmodified', {
+                  extensions: { custom: 'extension' },
+                });
+              });
+
+              await setupApolloServerAndFetchPair({
+                sendErrorsInTraces: {
+                  unmodified: true,
+                },
+              });
+
+              const result = await apolloFetch({
+                query: `{fieldWhichWillError}`,
+              });
+              expect(result.data).toEqual({
+                fieldWhichWillError: null,
+              });
+              expect(result.errors).toBeDefined();
+              expect(result.errors[0].message).toEqual('should be unmodified');
+              expect(throwError).toHaveBeenCalledTimes(1);
+
+              const reports = await reportIngress.promiseOfReports;
+              expect(reports.length).toBe(1);
+              const trace = Object.values(reports[0].tracesPerQuery)[0]
+                .trace![0] as Trace;
+
+              // There should be no error at the root, our error is a child.
+              expect(trace.root!.error).toStrictEqual([]);
+
+              // There should only be one child.
+              expect(trace.root!.child!.length).toBe(1);
+
+              // The child should maintain the path and message
+              expect(trace.root!.child![0].error).toMatchInlineSnapshot(`
+                Array [
+                  Object {
+                    "json": "{\\"message\\":\\"should be unmodified\\",\\"locations\\":[{\\"line\\":1,\\"column\\":2}],\\"path\\":[\\"fieldWhichWillError\\"],\\"extensions\\":{\\"custom\\":\\"extension\\"}}",
+                    "location": Array [
+                      Object {
+                        "column": 2,
+                        "line": 1,
+                      },
+                    ],
+                    "message": "should be unmodified",
+                  },
+                ]
+              `);
+            });
+
+            it('masked', async () => {
+              throwError.mockImplementationOnce(() => {
+                throw new GraphQLError('should be masked', {
+                  extensions: { custom: 'extension' },
+                });
+              });
+
+              await setupApolloServerAndFetchPair({
+                sendErrorsInTraces: {
+                  masked: true,
+                },
+              });
+
+              const result = await apolloFetch({
+                query: `{fieldWhichWillError}`,
+              });
+              expect(result.data).toEqual({
+                fieldWhichWillError: null,
+              });
+              expect(result.errors).toBeDefined();
+              expect(result.errors[0].message).toEqual('should be masked');
+              expect(throwError).toHaveBeenCalledTimes(1);
+
+              const reports = await reportIngress.promiseOfReports;
+              expect(reports.length).toBe(1);
+              const trace = Object.values(reports[0].tracesPerQuery)[0]
+                .trace![0] as Trace;
+
+              // There should be no error at the root, our error is a child.
+              expect(trace.root!.error).toStrictEqual([]);
+
+              // There should only be one child.
+              expect(trace.root!.child!.length).toBe(1);
+
+              // The child should maintain the path, but have its message masked
+              expect(trace.root!.child![0].error).toMatchInlineSnapshot(`
+                Array [
+                  Object {
+                    "json": "{\\"message\\":\\"<masked>\\",\\"locations\\":[{\\"line\\":1,\\"column\\":2}],\\"path\\":[\\"fieldWhichWillError\\"],\\"extensions\\":{\\"maskedBy\\":\\"ApolloServerPluginUsageReporting\\"}}",
+                    "location": Array [
+                      Object {
+                        "column": 2,
+                        "line": 1,
+                      },
+                    ],
+                    "message": "<masked>",
+                  },
+                ]
+              `);
+            });
           });
         });
       });
