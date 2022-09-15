6a8372513c3d8bdf09e65ee7baeb268797cce41a
Trevor Scheer <trevor.scheer@gmail.com>
2022-09-15 13:49:21 -0700

    Add lint config for mdx files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,21 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import'],
-  extends: ['plugin:import/typescript'],
+  ignorePatterns: ['**/*.js'],
   overrides: [
     {
+      extends: ['plugin:import/typescript'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint', 'import'],
       // Enable import/extensions on all TS files because our ESM builds require
       // you to specify local imports as full paths with extensions. We don't
       // need this on tests because Jest doesn't require it.
       files: ['**/*.ts'],
       excludedFiles: '**/__tests__/**/*.ts',
       rules: { 'import/extensions': ['error', 'ignorePackages'] },
+    },
+    {
+      extends: ['plugin:mdx/recommended', 'plugin:prettier/recommended'],
+      files: ['**/*.md?(x)'],
     },
   ],
 };
