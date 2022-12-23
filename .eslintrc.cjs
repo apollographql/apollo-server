0bd833f7b2560a40e5146185dce0c9ee8aaac752
Trevor Scheer <trevor.scheer@gmail.com>
2022-12-23 07:34:56 -0800

    migrate to lint plugin for .js imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,6 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import'],
-  extends: ['plugin:import/typescript'],
-  overrides: [
-    {
-      // Enable import/extensions on all TS files because our ESM builds require
-      // you to specify local imports as full paths with extensions. We don't
-      // need this on tests because Jest doesn't require it.
-      files: ['**/*.ts'],
-      excludedFiles: '**/__tests__/**/*.ts',
-      rules: { 'import/extensions': ['error', 'ignorePackages'] },
-    },
-  ],
+  plugins: ['@typescript-eslint', 'require-extensions'],
+  extends: ['plugin:require-extensions/recommended'],
 };
