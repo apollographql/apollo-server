b69f3dd89a6d6614492b6748f44ead34aec22f8b
Trevor Scheer <trevor.scheer@gmail.com>
2023-08-24 11:49:33 -0700

    prettier-fix

--- packages/server/src/ApolloServer.ts
+++ packages/server/src/ApolloServer.ts
@@ -502,8 +502,8 @@ export class ApolloServer<in out TContext extends BaseContext = BaseContext> {
 
       try {
         await Promise.all(
-          this.internals.plugins.map(
-            async (plugin) => plugin.startupDidFail?.({ error }),
+          this.internals.plugins.map(async (plugin) =>
+            plugin.startupDidFail?.({ error }),
           ),
         );
       } catch (pluginError) {
@@ -1057,11 +1057,10 @@ export class ApolloServer<in out TContext extends BaseContext = BaseContext> {
         const error = ensureError(maybeError);
         try {
           await Promise.all(
-            this.internals.plugins.map(
-              async (plugin) =>
-                plugin.contextCreationDidFail?.({
-                  error,
-                }),
+            this.internals.plugins.map(async (plugin) =>
+              plugin.contextCreationDidFail?.({
+                error,
+              }),
             ),
           );
         } catch (pluginError) {
@@ -1094,9 +1093,8 @@ export class ApolloServer<in out TContext extends BaseContext = BaseContext> {
       ) {
         try {
           await Promise.all(
-            this.internals.plugins.map(
-              async (plugin) =>
-                plugin.invalidRequestWasReceived?.({ error: maybeError }),
+            this.internals.plugins.map(async (plugin) =>
+              plugin.invalidRequestWasReceived?.({ error: maybeError }),
             ),
           );
         } catch (pluginError) {
@@ -1318,12 +1316,11 @@ export async function internalExecuteOperation<TContext extends BaseContext>(
     // If *these* hooks throw then we'll still get a 500 but won't mask its
     // error.
     await Promise.all(
-      internals.plugins.map(
-        async (plugin) =>
-          plugin.unexpectedErrorProcessingRequest?.({
-            requestContext,
-            error,
-          }),
+      internals.plugins.map(async (plugin) =>
+        plugin.unexpectedErrorProcessingRequest?.({
+          requestContext,
+          error,
+        }),
       ),
     );
     // Mask unexpected error externally.
