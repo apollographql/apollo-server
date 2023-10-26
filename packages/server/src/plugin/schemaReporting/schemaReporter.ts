4575810686a807cd6467e4838e2dad70e00ca4e3
cwikla <cwikla@valenciadata.com>
2023-10-26 14:17:32 -0700

    Change from ReturnType<> to NodeJS.Timeout

--- packages/server/src/plugin/schemaReporting/schemaReporter.ts
+++ packages/server/src/plugin/schemaReporting/schemaReporter.ts
@@ -40,7 +40,7 @@ export class SchemaReporter {
   private readonly fetcher: Fetcher;
 
   private isStopped: boolean;
-  private pollTimer?: ReturnType<typeof setTimeout>;
+  private pollTimer?: NodeJS.Timeout;
   private readonly headers: Record<string, string>;
 
   constructor(options: {
