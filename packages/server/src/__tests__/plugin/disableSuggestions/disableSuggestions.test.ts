43963685871f5cf8738f2091d64b36a6f2cc845a
Andrew McGivery <andrew.mcgivery@apollographql.com>
2024-08-07 18:05:07 -0400

    Ignore purposeful spelling mistakes

--- packages/server/src/__tests__/plugin/disableSuggestions/disableSuggestions.test.ts
+++ packages/server/src/__tests__/plugin/disableSuggestions/disableSuggestions.test.ts
@@ -44,15 +44,18 @@ describe('ApolloServerPluginDisableSuggestions', () => {
   it('should not hide suggestions when plugin is not enabled', async () => {
     const response = await makeServer({
       withPlugin: false,
+      // cSpell:disable
       query: `#graphql
             query {
               helloo
             }
           `,
+      // cSpell:enable
     });
 
     assert(response.body.kind === 'complete');
     expect(JSON.parse(response.body.string).errors[0].message).toBe(
+      // cspell:disable-next-line
       'Cannot query field "helloo" on type "Query". Did you mean "hello"?',
     );
   });
@@ -60,15 +63,18 @@ describe('ApolloServerPluginDisableSuggestions', () => {
   it('should hide suggestions when plugin is enabled', async () => {
     const response = await makeServer({
       withPlugin: true,
+      // cSpell:disable
       query: `#graphql
             query {
               helloo
             }
           `,
+      // cSpell:enable
     });
 
     assert(response.body.kind === 'complete');
     expect(JSON.parse(response.body.string).errors[0].message).toBe(
+      // cspell:disable-next-line
       'Cannot query field "helloo" on type "Query".',
     );
   });
