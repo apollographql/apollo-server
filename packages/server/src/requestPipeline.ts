0015a2aad668885074fd7fe21bf13885160dc339
Andrew McGivery <andrew.mcgivery@apollographql.com>
2024-08-08 17:01:46 -0400

    Allow formatError to be an async function

--- packages/server/src/requestPipeline.ts
+++ packages/server/src/requestPipeline.ts
@@ -474,7 +474,7 @@ export async function processGraphQLRequest<TContext extends BaseContext>(
       }
 
       const { formattedErrors, httpFromErrors } = resultErrors
-        ? formatErrors(resultErrors)
+        ? await formatErrors(resultErrors)
         : { formattedErrors: undefined, httpFromErrors: newHTTPGraphQLHead() };
 
       // TODO(AS5) This becomes the default behavior and the
@@ -592,7 +592,7 @@ export async function processGraphQLRequest<TContext extends BaseContext>(
                       // Note that any `http` extensions in errors have no
                       // effect, because we've already sent the status code
                       // and response headers.
-                      errors: formatErrors(errors).formattedErrors,
+                      errors: (await formatErrors(errors)).formattedErrors,
                     };
                   }
                   return incrementalResult;
@@ -654,7 +654,7 @@ export async function processGraphQLRequest<TContext extends BaseContext>(
   ): Promise<GraphQLResponse> {
     await didEncounterErrors(errors);
 
-    const { formattedErrors, httpFromErrors } = formatErrors(errors);
+    const { formattedErrors, httpFromErrors } = await formatErrors(errors);
 
     requestContext.response.body = {
       kind: 'single',
