d718631df1ef9a271f060458a55c0c2d0941e6ed
Yaacov Rydzinski <yaacovCR@gmail.com>
2025-06-20 05:43:09 +0300

    add demonstration of dev mode

--- packages/server/src/__tests__/testGraphqlESM.test.ts
+++ packages/server/src/__tests__/testGraphqlESM.test.ts
@@ -1,8 +1,23 @@
 import { describe, it, expect } from '@jest/globals';
-import { version } from 'graphql17';
+import 'graphql17/dev';
+import { GraphQLObjectType, isObjectType, version } from 'graphql17';
 
 describe('canary ESM graphql v17-alpha', () => {
   it('should work', () => {
     expect(version).toBe('17.0.0-alpha.9'); // this version is the latest alpha on which the canary is based
+  });
+
+  it('should use development mode when set by our Jest Config', () => {
+    function getFakeGraphQLObjectType() {
+      class GraphQLObjectType {
+        get [Symbol.toStringTag]() {
+          return 'GraphQLObjectType';
+        }
+      }
+      return new GraphQLObjectType();
+    }
+
+    expect(isObjectType(new GraphQLObjectType({ name: 'SomeType', fields: {} }))).toBe(true);
+    expect(() => isObjectType(getFakeGraphQLObjectType())).toThrow();
   });
 });
